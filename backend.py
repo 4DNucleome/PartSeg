Add possibility to print statistic horizontal add calculation all statistics bugfix in statistic load

backend.py
@@ -133,7 +133,8 @@ def bisect(arr, val, comp):
     return r
 
 SettingsValue = namedtuple("SettingsValue", ["function_name", "help_message", "arguments"])
-
+Leaf = namedtuple("Leaf", ["name", "dict"])
+Node = namedtuple("Node", ["left", 'op', 'right'])
 
 class StatisticProfile(object):
 
@@ -165,23 +166,112 @@ class StatisticProfile(object):
         self.name = name
         self.chosen_fields = []
         for cf_val in chosen_fields:
-            name = cf_val[0]
             user_name = cf_val[1]
-            sp = name.split("[")
-            if len(sp) == 1 and self.STATISTIC_DICT[sp[0]].arguments is None:
-                self.chosen_fields.append((name, user_name, None))
-            elif len(sp) == 2 and self.STATISTIC_DICT[sp[0]].arguments is not None:
-                arguments = sp[1][:-1].split(",")
-                params = dict()
-                for arg in arguments:
-                    val_name, val = arg.split("=")
-                    val = self.STATISTIC_DICT[sp[0]].arguments[val_name](val)
-                    params[val_name] = val
-                self.chosen_fields.append((sp[0], user_name, params))
-            elif len(sp) == 1 and len(cf_val) == 3 and self.STATISTIC_DICT[sp[0]].arguments is not None:
-                self.chosen_fields.append((sp[0], user_name, cf_val[2]))
+            if isinstance(cf_val[0], str):
+                tree = self.parse_statistic(cf_val[0])
+            else:
+                tree = self.rebuild_tree(cf_val[0])
+            self.chosen_fields.append((tree, user_name, None))
         self.settings = settings
         self.reversed_brightness = reversed_brightness
+
+    def rebuild_tree(self, l):
+        if len(l) == 2:
+            return Leaf(*l)
+        else:
+            return Node(self.rebuild_tree(l[0]), l[1], self.rebuild_tree(l[2]))
+
+    @staticmethod
+    def tokenize(text):
+        special = ["(", ")", "[", "]", "/", "+", ","]
+        res = []
+        temp_str = ""
+        for l in text:
+            if l in special:
+                if temp_str != "":
+                    res.append(temp_str)
+                    temp_str = ""
+                res.append(l)
+            else:
+                temp_str += l
+        if temp_str != "":
+            res.append(temp_str)
+        return res
+
+    def build_tree(self, tokens):
+        res = []
+        final_res = res
+        pos = 0
+        while True:
+            if pos == len(tokens):
+                break
+            if tokens[pos] == ")":
+                pos += 1
+                break
+            if tokens[pos] == "/":
+                final_res = [res[:], "/"]
+                res = []
+                final_res.append(res)
+                pos += 1
+            if tokens[pos] in "[],":
+                pos += 1
+                continue
+            if tokens[pos] == "(":
+                sub_tree, pos_shift = self.build_tree(tokens[pos+1:])
+                pos += pos_shift+1
+                res.extend(sub_tree)
+                continue
+            res.append(tokens[pos])
+            pos += 1
+        return final_res, pos
+
+    def tree_to_dict_tree(self, tree):
+        if isinstance(tree[0],list):
+            left_tree = self.tree_to_dict_tree(tree[0])
+            right_tree = self.tree_to_dict_tree(tree[2])
+            return Node(left_tree, tree[1], right_tree)
+        else:
+            name = tree[0]
+            base_stat = self.STATISTIC_DICT[name]
+            d = dict()
+            for el in tree[1:]:
+                sp = el.split("=")
+                d[sp[0]] = base_stat.arguments[sp[0]](sp[1])
+            return Leaf(name, d)
+
+    def parse_statistic(self, text):
+        tokens = self.tokenize(text)
+        if tokens[0] == "(":
+            pos = 1
+        else:
+            pos = 0
+        tree, l = self.build_tree(tokens)
+        return self.tree_to_dict_tree(tree)
+
+    def calculate_tree(self, node, help_dict, kwargs):
+        """
+        :type node: Leaf | Node
+        :type kwargs: dict
+        :return: float
+        """
+        if isinstance(node, Leaf):
+            fun_name = self.STATISTIC_DICT[node.name][0]
+            kw = dict(kwargs)
+            kw.update(node.dict)
+            hash_str = "{}: {}".format(fun_name, kw)
+            if hash_str in help_dict:
+                return help_dict[hash_str]
+            fun = getattr(self, fun_name)
+            val = fun(**kw)
+            help_dict[hash_str] = val
+            return val
+        elif isinstance(node, Node):
+            left_res = self.calculate_tree(node.left, help_dict, kwargs)
+            right_res = self.calculate_tree(node.right, help_dict, kwargs)
+            if node.op == "/":
+                return left_res/right_res
+        logging.error("Wrong statistics: {}".format(node))
+        return 1
 
     def calculate(self, image, mask, full_mask, base_mask):
         result = OrderedDict()
@@ -189,43 +279,10 @@ class StatisticProfile(object):
         if self.reversed_brightness:
             noise_mean = np.mean(image[full_mask == 0])
             image = noise_mean - image
-        for name, user_name, params in self.chosen_fields:
-            print(name, )
-            if name in self.STATISTIC_DICT:
-                fun_name, _, params = self.STATISTIC_DICT[name]
-                print(fun_name)
-                fun = getattr(self, fun_name)
-                kw = {"image": image, "mask": mask, "base_mask": base_mask, "full_mask": full_mask}
-                if params is not None:
-                    kw.update(params)
-                result[user_name] = fun(**kw)
-            else:
-                result[user_name] = 1
-        for name, user_name, params in self.chosen_fields:
-            if name not in self.STATISTIC_DICT:
-                names = name.split("/")
-
-                def get_val(sub_name):
-                    if sub_name in result:
-                        val = result[names[0]]
-                    elif sub_name in support_dict:
-                        val = support_dict[names[0]]
-                    else:
-                        gv_fun_name, _, _ = self.STATISTIC_DICT[sub_name]
-                        gv_fun = getattr(self, gv_fun_name)
-                        gv_kw = {"image": image, "mask": mask, "base_mask": base_mask, "full_mask": full_mask}
-                        val = gv_fun(**gv_kw)
-                        support_dict[user_name] = val
-                    return val
-                stat = get_val(names[0])
-                try:
-                    for n in names[1:]:
-                        stat /= get_val(n)
-                except ZeroDivisionError:
-                    stat = None
-                except TypeError:
-                    stat = None
-                result[user_name] = stat
+        help_dict = dict()
+        kw = {"image": image, "mask": mask, "base_mask": base_mask, "full_mask": full_mask}
+        for tree, user_name, params in self.chosen_fields:
+            result[user_name] = self.calculate_tree(tree, help_dict, kw)
         return result
 
     @staticmethod
@@ -296,7 +353,7 @@ class StatisticProfile(object):
             return None
         img = np.copy(image)
         img[mask == 0] = 0
-        return af.calculate_density_momentum(img, self.settings.voxel_size)
+        return af.calculate_density_momentum(img, self.settings.voxel_size,)
 
 
 class Profile(object):

gui.py
@@ -1215,6 +1215,7 @@ class StatisticsWindow(QWidget):
         self.recalculate_button = QPushButton("Recalculate statistics", self)
         self.recalculate_button.clicked.connect(self.update_statistics)
         self.copy_button = QPushButton("Copy to clipboard", self)
+        self.horizontal_statistics = QCheckBox("Horizontal", self)
         self.copy_button.clicked.connect(self.copy_to_clipboard)
         self.statistic_type = QComboBox(self)
         self.statistic_type.addItem("Emish statistics (oryginal)")
@@ -1230,8 +1231,9 @@ class StatisticsWindow(QWidget):
         butt_layout.setMargin(0)
         print(butt_layout.spacing())
         butt_layout.setSpacing(10)
-        butt_layout.addWidget(self.copy_button)
-        butt_layout.addWidget(self.statistic_type)
+        butt_layout.addWidget(self.horizontal_statistics, 1)
+        butt_layout.addWidget(self.copy_button, 2)
+        butt_layout.addWidget(self.statistic_type, 2)
         v_butt_layout.addWidget(self.recalculate_button)
         v_butt_layout.addLayout(butt_layout)
         layout.addLayout(v_butt_layout)
@@ -1258,14 +1260,7 @@ class StatisticsWindow(QWidget):
         if self.statistic_type.currentText() == "Emish statistics (oryginal)":
             image, mask = get_segmented_data(np.copy(self.settings.image), self.settings, self.segment)
             stat = calculate_statistic_from_image(image, mask, self.settings)
-            self.info_field.setRowCount(len(stat))
-            for i, (key, val) in enumerate(stat.items()):
-                self.info_field.setItem(i, 0, QTableWidgetItem(key))
-                self.info_field.setItem(i, 1, QTableWidgetItem(str(val)))
-                try:
-                    self.info_field.setItem(i, 2, QTableWidgetItem(UNITS_DICT[key].format(self.settings.size_unit)))
-                except KeyError as k:
-                    logging.warning(k.message)
+
         else:
             compute_class = self.settings.statistics_profile_dict[str(self.statistic_type.currentText())]
             if self.settings.use_gauss:
@@ -1276,16 +1271,32 @@ class StatisticsWindow(QWidget):
             full_mask = self.segment.get_full_segmentation()
             base_mask = self.settings.mask
             stat = compute_class.calculate(image, mask, full_mask, base_mask)
+        if self.horizontal_statistics.isChecked():
+            self.info_field.setRowCount(3)
+            self.info_field.setColumnCount(len(stat))
+            self.info_field.setVerticalHeaderLabels(["Name", "Value", "Units"])
+            self.info_field.setHorizontalHeaderLabels([str(x) for x in range(len(stat))])
+            for i, (key, val) in enumerate(stat.items()):
+                print(i, key, val)
+                self.info_field.setItem(0, i, QTableWidgetItem(key))
+                self.info_field.setItem(1, i, QTableWidgetItem(str(val)))
+                try:
+                    self.info_field.setItem(2, i, QTableWidgetItem(UNITS_DICT[key].format(self.settings.size_unit)))
+                except KeyError as k:
+                    logging.warning(k.message)
+        else:
             self.info_field.setRowCount(len(stat))
+            self.info_field.setColumnCount(3)
+            self.info_field.setVerticalHeaderLabels([str(x) for x in range(len(stat))])
+            self.info_field.setHorizontalHeaderLabels(["Name", "Value", "Units"])
             for i, (key, val) in enumerate(stat.items()):
                 print(i, key, val)
                 self.info_field.setItem(i, 0, QTableWidgetItem(key))
                 self.info_field.setItem(i, 1, QTableWidgetItem(str(val)))
-                self.info_field.setItem(i, 2, QTableWidgetItem(""))
-                #try:
-                #    self.info_field.setItem(i, 2, QTableWidgetItem(UNITS_DICT[key].format(self.settings.size_unit)))
-                #except KeyError as k:
-                #    logging.warning(k.message)
+                try:
+                    self.info_field.setItem(i, 2, QTableWidgetItem(UNITS_DICT[key].format(self.settings.size_unit)))
+                except KeyError as k:
+                    logging.warning(k.message)
 
     def keyPressEvent(self, e):
         if e.modifiers() & Qt.ControlModifier:
@@ -1608,13 +1619,19 @@ class StatisticsSettings(QWidget):
     def choose_element(self):
         if self.chosen_element is None:
             item = self.profile_options.currentItem()
-            item.setIcon(QIcon("icons/task-accepted.png"))
+            item.setIcon(QIcon(os.path.join("icons", "task-accepted.png")))
             self.chosen_element = item
         elif self.profile_options.currentItem() == self.chosen_element:
             self.chosen_element.setIcon(QIcon())
             self.chosen_element = None
         else:
-            lw = QListWidgetItem("{}/{}".format(self.chosen_element.text(), self.profile_options.currentItem().text()))
+            text1 = self.chosen_element.text()
+            if "/" in text1:
+                text1 = "({})".format(text1)
+            text2 = self.profile_options.currentItem().text()
+            if "/" in text2:
+                text2 = "({})".format(text2)
+            lw = QListWidgetItem("{}/{}".format(text1, text2))
             lw.setToolTip("User defined")
             self.profile_options_chosen.addItem(lw)
             self.chosen_element.setIcon(QIcon())
@@ -1666,7 +1683,10 @@ class StatisticsSettings(QWidget):
     def choose_option(self):
         selected_item = self.profile_options.currentItem()
         selected_row = self.profile_options.currentRow()
-        arguments = StatisticProfile.STATISTIC_DICT[str(selected_item.text())].arguments
+        if str(selected_item.text()) in StatisticProfile.STATISTIC_DICT:
+            arguments = StatisticProfile.STATISTIC_DICT[str(selected_item.text())].arguments
+        else:
+            arguments = None
         if arguments is not None:
             val_dialog = MultipleInput("Set parameters:",
                                        StatisticProfile.STATISTIC_DICT[str(selected_item.text())].help_message,
@@ -1817,22 +1837,14 @@ class MainMenu(QWidget):
         self.settings.add_image_callback(self.set_layer_threshold)
         self.settings.add_change_layer_callback(self.changed_layer)
         self.load_button = QPushButton(self)
-        if QIcon.hasThemeIcon("document-open") and False:
-            self.load_button.setIcon(QIcon.fromTheme("document-open"))
-        else:
-            # self.load_button.setText("Open")
-            self.load_button.setIcon(QIcon("icons/document-open.png"))
-            self.load_button.setStyleSheet("padding: 3px;")
-            self.load_button.setToolTip("Open")
+        self.load_button.setIcon(QIcon(os.path.join("icons", "document-open.png")))
+        self.load_button.setStyleSheet("padding: 3px;")
+        self.load_button.setToolTip("Open")
         self.load_button.clicked.connect(self.open_file)
         self.save_button = QPushButton(self)
-        if QIcon.hasThemeIcon("document-save") and False:
-            self.save_button.setIcon(QIcon.fromTheme("document-save"))
-        else:
-            self.save_button.setIcon(QIcon("icons/document-save-as.png"))
-            self.save_button.setStyleSheet("padding: 3px;")
-            self.save_button.setToolTip("Save")
-            # self.save_button.setText("Save")
+        self.save_button.setIcon(QIcon(os.path.join("icons", "document-save-as.png")))
+        self.save_button.setStyleSheet("padding: 3px;")
+        self.save_button.setToolTip("Save")
         self.save_button.setDisabled(True)
         self.save_button.clicked.connect(self.save_file)
         self.mask_button = QPushButton("Mask manager", self)
@@ -1871,7 +1883,7 @@ class MainMenu(QWidget):
         self.profile_choose.addItems(list(self.settings.get_profile_list()))
         self.profile_choose.currentIndexChanged[str_type].connect(self.profile_changed)
         self.advanced_button = QPushButton(self)  # "Advanced"
-        self.advanced_button.setIcon(QIcon("icons/configure.png"))
+        self.advanced_button.setIcon(QIcon(os.path.join("icons", "configure.png")))
         self.advanced_button.setStyleSheet("padding: 3px;")
         self.advanced_button.setToolTip("Advanced settings and statistics")
         self.advanced_button.clicked.connect(self.open_advanced)