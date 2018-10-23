401aea279d76e1c1ceb30984077a2bf0c776975a
Grzegorz Bokota <bokota+github@gmail.com>
2018-10-23 18:16:19 +0200

    workaround for segfault on close

--- src/stackseg_main.py
+++ src/stackseg_main.py
@@ -1,6 +1,13 @@
 import sys
 __author__ = "Grzegorz Bokota"
 
+import os
+import signal
+
+def sig_handler(signum, frame):
+    print ("segfault")
+
+# signal.signal(signal.SIGSEGV, sig_handler)
 
 def my_excepthook(type, value, tback):
     # log the exception here
@@ -15,12 +22,14 @@ def main():
     import logging
     logging.basicConfig(level=logging.INFO)
     from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtCore import QThread
     from stackseg.stack_gui_main import MainWindow
     import sys
     myApp = QApplication(sys.argv)
     wind = MainWindow("StackSeg")
     wind.show()
     myApp.exec_()
+    del wind
     sys.exit()
 
 if __name__ == '__main__':
