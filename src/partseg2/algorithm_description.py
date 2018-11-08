1e696ea59b72e41c5f37f2b48078403760b91f8b
Grzegorz Bokota <bokota+github@gmail.com>
2018-11-08 10:55:04 +0100

    clean algorithm description files

--- src/partseg2/algorithm_description.py
+++ src/partseg2/algorithm_description.py
@@ -1,45 +1,6 @@
 from project_utils.segmentation.restartable_segmentation_algorithms import LowerThresholdAlgorithm, UpperThresholdAlgorithm, RangeThresholdAlgorithm, \
     LowerThresholdDistanceFlowAlgorithm, UpperThresholdDistanceFlowAlgorithm, LowerThresholdPathFlowAlgorithm, \
     UpperThresholdPathFlowAlgorithm, UpperThresholdPathDistanceFlowAlgorithm, LowerThresholdPathDistanceFlowAlgorithm
-from project_utils.algorithms_description import AlgorithmProperty
-from copy import deepcopy
-
-from project_utils.image_operations import RadiusType
-
-
-lower_threshold_algorithm = [AlgorithmProperty("threshold", "Threshold", 10000, (0, 10 ** 6), 100),
-                             AlgorithmProperty("minimum_size", "Minimum size (pix)", 8000, (0, 10 ** 6), 1000),
-                             AlgorithmProperty("use_gauss", "Use gauss", RadiusType.NO, None),
-                             AlgorithmProperty("gauss_radius", "Gauss radius", 1.0, (0, 10), 0.1),
-                             AlgorithmProperty("side_connection", "Connect only sides", False, (True, False))]
-
-upper_threshold_algorithm = deepcopy(lower_threshold_algorithm)
-
-range_threshold_algorithm = [AlgorithmProperty("lower_threshold", "Lower threshold", 10000, (0, 10 ** 6), 100),
-                             AlgorithmProperty("upper_threshold", "Upper threshold", 10000, (0, 10 ** 6), 100),
-                             AlgorithmProperty("minimum_size", "Minimum size (pix)", 8000, (0, 10 ** 6), 1000),
-                             AlgorithmProperty("use_gauss", "Use gauss", RadiusType.NO, None),
-                             AlgorithmProperty("gauss_radius", "Gauss radius", 1.0, (0, 10), 0.1),
-                             AlgorithmProperty("side_connection", "Connect only sides", False, (True, False))]
-
-base_flow_threshold_algorithm = deepcopy(lower_threshold_algorithm)
-base_flow_threshold_algorithm.insert(1, AlgorithmProperty("base_threshold", "Base threshold", 10000, (0, 10 ** 6), 100))
-
-# noinspection PyTypeChecker
-upper_euclidean_flow_threshold_algorithm = ["May not work proper when voxel is not cube"] + deepcopy(
-    base_flow_threshold_algorithm)
-
-# noinspection PyTypeChecker
-lower_euclidean_flow_threshold_algorithm = ["May not work proper when voxel is not cube"] + deepcopy(
-    base_flow_threshold_algorithm)
-
-lower_path_flow_threshold_algorithm = deepcopy(base_flow_threshold_algorithm)
-
-upper_path_flow_threshold_algorithm = deepcopy(base_flow_threshold_algorithm)
-
-lower_path_euclidean_flow_threshold_algorithm = deepcopy(lower_euclidean_flow_threshold_algorithm)
-
-upper_path_euclidean_flow_threshold_algorithm = deepcopy(upper_euclidean_flow_threshold_algorithm)
 
 part_algorithm_dict = {
     "Lower threshold": LowerThresholdAlgorithm,
--- src/stackseg/stack_algorithm/algorithm_description.py
+++ src/stackseg/stack_algorithm/algorithm_description.py
@@ -1,33 +1,5 @@
-from copy import deepcopy
-
-from project_utils.algorithms_description import AlgorithmProperty
-from project_utils.image_operations import RadiusType
-
 from project_utils.segmentation.segmentation_algorithm import ThresholdAlgorithm, ThresholdPreview, \
     AutoThresholdAlgorithm
-
-
-
-
-only_threshold_algorithm = [AlgorithmProperty("threshold", "Threshold", 1000, (0, 10 ** 6), 100),
-                            AlgorithmProperty("use_gauss", "Use gauss", RadiusType.NO, None),
-                            AlgorithmProperty("gauss_radius", "Gauss radius", 1.0, (0, 10), 0.1)]
-
-threshold_algorithm = [AlgorithmProperty("threshold", "Threshold", 10000, (0, 10 ** 6), 100),
-                       AlgorithmProperty("minimum_size", "Minimum size", 8000, (20, 10 ** 6), 1000),
-                       AlgorithmProperty("close_holes", "Close small holes", True, (True, False)),
-                       AlgorithmProperty("close_holes_size", "Small holes size", 200, (0, 10**3), 10),
-                       AlgorithmProperty("smooth_border", "Smooth borders", True, (True, False)),
-                       AlgorithmProperty("smooth_border_radius", "Smooth borders radius", 2, (0, 20), 1),
-                       AlgorithmProperty("use_gauss", "Use gauss", RadiusType.NO, None),
-                       AlgorithmProperty("gauss_radius", "Gauss radius", 1.0, (0, 10), 0.1),
-                       AlgorithmProperty("side_connection", "Connect only sides", False, (True, False)),
-                       AlgorithmProperty("use_convex", "Use convex_hull", False, (True, False))]
-
-auto_threshold_algorithm = deepcopy(threshold_algorithm)
-
-auto_threshold_algorithm.insert(0, AlgorithmProperty("suggested_size", "Suggested size", 200000, (0, 10 ** 6), 1000))
-
 
 stack_algorithm_dict = {
     "Threshold": ThresholdAlgorithm,
