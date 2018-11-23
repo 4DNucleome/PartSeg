89c04ada83f3537a292ae2e380e1a2ed89e8ee79
Grzegorz Bokota - Vmachine <g.bokota@cent.uw.edu.pl>
2018-11-23 17:35:10 +0100

    midify for start on windows.

--- src/launcher_main.py
+++ src/launcher_main.py
@@ -8,21 +8,24 @@ from project_utils.base_argparser import CustomParser
 from project_utils.except_hook import my_excepthook
 
 sys.excepthook = my_excepthook
-
+print("buka")
 def main():
     parser = CustomParser("PartSeg")
+    parser.add_argument("--multiprocessing-fork", dest="mf", action="store_true",
+                        help=argparse.SUPPRESS) # Windows bugfix
     sp = parser.add_subparsers()
     sp_a = sp.add_parser("analysis", help="Starts GUI for analysis")
     sp_s = sp.add_parser("segmentation", help="Starts GUI for segmentation")
     parser.set_defaults(gui="launcher")
     sp_a.set_defaults(gui="analysis")
     sp_s.set_defaults(gui="segmentation")
-    args = parser.parse_args()
+    argv = [x for x in sys.argv[1:] if not (x.startswith("parent") or x.startswith("pipe"))]
+    args = parser.parse_args(argv)
     print(args)
 
     logging.basicConfig(level=logging.INFO)
     myApp = QApplication(sys.argv)
-    if args.gui == "analysis":
+    if args.gui == "analysis" or args.mf:
         from partseg2.main_window import MainWindow
         title = "PartSeg"
     elif args.gui == "segmentation":
