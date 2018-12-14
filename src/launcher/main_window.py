fcf77cb9076997682bc30f3110afb250fb3524c8
Grzegorz Bokota <bokota+github@gmail.com>
2018-12-14 11:45:40 +0100

    show reaction on button click in launcher

--- src/launcher/main_window.py
+++ src/launcher/main_window.py
@@ -1,44 +1,66 @@
 import os
-
-from PyQt5.QtCore import QSize, Qt
+import importlib
+from PyQt5.QtCore import QSize, Qt, QTimerEvent
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QToolButton, QHBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QToolButton, QGridLayout, QWidget, QProgressBar
 from partseg_utils.global_settings import static_file_folder
 
-
+import time
 class MainWindow(QMainWindow):
     def __init__(self, title):
         super().__init__()
         self.setWindowTitle(title)
+        self.lib_path = ""
+        self.final_title = ""
         analysis_icon = QIcon(os.path.join(static_file_folder, 'icons', "icon.png"))
         stack_icon = QIcon(os.path.join(static_file_folder, 'icons', "icon_stack.png"))
-        self.partseg_button = QToolButton(self)
-        self.partseg_button.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
-        self.partseg_button.setIcon(analysis_icon)
-        self.partseg_button.setText("Segmentation\nAnalysis")
-        self.partseg_button.setIconSize(QSize(100, 100))
-        self.stackseg_button = QToolButton(self)
-        self.stackseg_button.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
-        self.stackseg_button.setIcon(stack_icon)
-        self.stackseg_button.setText("Mask\nSegmentation")
-        self.stackseg_button.setIconSize(QSize(100, 100))
-        self.partseg_button.clicked.connect(self.launch_partseg)
-        self.stackseg_button.clicked.connect(self.launch_stackseg)
-        layout = QHBoxLayout()
-        layout.addWidget(self.partseg_button)
-        layout.addWidget(self.stackseg_button)
+        self.analysis_button = QToolButton(self)
+        self.analysis_button.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        self.analysis_button.setIcon(analysis_icon)
+        self.analysis_button.setText("Segmentation\nAnalysis")
+        self.analysis_button.setIconSize(QSize(100, 100))
+        self.mask_button = QToolButton(self)
+        self.mask_button.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        self.mask_button.setIcon(stack_icon)
+        self.mask_button.setText("Mask\nSegmentation")
+        self.mask_button.setIconSize(QSize(100, 100))
+        self.analysis_button.clicked.connect(self.launch_analysis)
+        self.mask_button.clicked.connect(self.launch_mask)
+        self.progress = QProgressBar()
+        self.progress.setHidden(True)
+        layout = QGridLayout()
+        layout.addWidget(self.progress, 0, 0, 1, 2)
+        layout.addWidget(self.analysis_button, 1, 0)
+        layout.addWidget(self.mask_button, 1, 1)
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
         self.setWindowIcon(analysis_icon)
 
-    def launch_partseg(self):
-        from segmentation_analysis.main_window import MainWindow
-        self.launch(MainWindow, "PartSeg Segmentation Analysis")
+    def launch_analysis(self):
+        self.progress.setVisible(True)
+        self.progress.setRange(0, 0)
+        self.analysis_button.setDisabled(True)
+        self.mask_button.setDisabled(True)
+        self.lib_path = "segmentation_analysis.main_window"
+        self.final_title = "PartSeg Segmentation Analysis"
+        self.startTimer(0)
 
-    def launch_stackseg(self):
-        from stackseg.stack_gui_main import MainWindow
-        self.launch(MainWindow, "PartSeg Mask Segmentation")
+    def timerEvent(self, a0: 'QTimerEvent'):
+        self.killTimer(a0.timerId())
+        if self.lib_path != "":
+            main_window_module = importlib.import_module(self.lib_path)
+            self.launch(main_window_module.MainWindow, self.final_title)
+
+
+    def launch_mask(self):
+        self.progress.setVisible(True)
+        self.progress.setRange(0, 0)
+        self.analysis_button.setDisabled(True)
+        self.mask_button.setDisabled(True)
+        self.lib_path = "stackseg.stack_gui_main"
+        self.final_title = "PartSeg Mask Segmentation"
+        self.startTimer(0)
 
     def window_shown(self):
         self.close()
