7d8d4ba376bc9cce68440b50c2b278252605de82
Grzegorz Bokota <g.bokota@cent.uw.edu.pl>
2019-01-30 23:49:37 +0100

    fix in travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/cache/
 wget -N https://4dnucleome.cent.uw.edu.pl/PartSeg/Downloads/test_data.tbz2 -P ~/cache/
 
-tar -jxf ~/cache/test_data.tbz2 -c .
+tar -jxf ~/cache/test_data.tbz2 -C .
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
