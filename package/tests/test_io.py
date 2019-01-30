ac371e0f2400fe88ac4853e56b1676e07628d54b
Grzegorz Bokota <g.bokota@cent.uw.edu.pl>
2019-01-30 23:18:00 +0100

    Add more statistic basic test

--- package/tests/test_io.py
+++ package/tests/test_io.py
@@ -4,8 +4,10 @@ import os.path
 import numpy as np
 import pytest
 
-class TestImageClass():
-    def get_test_dir(self):
+
+class TestImageClass:
+    @staticmethod
+    def get_test_dir():
         return os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), "test_data")
 
     def test_image_read(self):
@@ -20,11 +22,10 @@ class TestImageClass():
         with pytest.raises(ValueError):
             Image(np.zeros((1, 10, 50, 50, 4)), [5, 5, 5], mask=np.zeros((1, 10, 50, 50, 4)))
 
-
     def test_read_with_mask(self):
         test_dir = self.get_test_dir()
         image = ImageReader.read_image(os.path.join(test_dir, "stack1_components", "stack1_component1.tif"),
-                                        os.path.join(test_dir, "stack1_components", "stack1_component1_mask.tif"))
+                                       os.path.join(test_dir, "stack1_components", "stack1_component1_mask.tif"))
         assert isinstance(image, Image)
         with pytest.raises(ValueError):
             ImageReader.read_image(os.path.join(test_dir, "stack1_components", "stack1_component1.tif"),
@@ -41,11 +42,9 @@ class TestImageClass():
 
     def _test_ome_read(self):  # error in tifffile
         test_dir = self.get_test_dir()
-        test_dir = self.get_test_dir()
         image1 = ImageReader.read_image(os.path.join(test_dir, "test_lsm2.tif"))
         image2 = ImageReader.read_image(os.path.join(test_dir, "test_lsm.tif"))
         data = np.load(os.path.join(test_dir, "test_lsm.npy"))
         assert np.all(image1.get_data() == data)
         assert np.all(image2.get_data() == data)
         assert np.all(image1.get_data() == image2.get_data())
-
--- package/tests/test_statistics.py
+++ package/tests/test_statistics.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 from PartSeg.tiff_image import Image
-from PartSeg.utils.analysis.statistics_calculation import Diameter, PixelBrightnessSum, Volume, ComponentsNumber
+from PartSeg.utils.analysis.statistics_calculation import Diameter, PixelBrightnessSum, Volume, ComponentsNumber, \
+    MaximumPixelBrightness, MinimumPixelBrightness, MeanPixelBrightness, MedianPixelBrightness, \
+    StandardDeviationOfPixelBrightness, MomentOfInertia
 
 
 def get_cube_array():
@@ -20,8 +22,10 @@ class TestDiameter(object):
         image = get_cube_image()
         mask1 = image.get_channel(0) > 40
         mask2 = image.get_channel(0) > 60
+        mask3 = mask1 * ~mask2
         assert Diameter.calculate_property(mask1, image.spacing, 1) == np.sqrt(2 * (50 * 59) ** 2 + (100 * 29) ** 2)
         assert Diameter.calculate_property(mask2, image.spacing, 1) == np.sqrt(2 * (50 * 39) ** 2 + (100 * 19) ** 2)
+        assert Diameter.calculate_property(mask3, image.spacing, 1) == np.sqrt(2 * (50 * 59) ** 2 + (100 * 29) ** 2)
 
 
 class TestPixelBrightnessSum(object):
@@ -29,9 +33,11 @@ class TestPixelBrightnessSum(object):
         image = get_cube_image()
         mask1 = image.get_channel(0) > 40
         mask2 = image.get_channel(0) > 60
+        mask3 = mask1 * ~mask2
         assert PixelBrightnessSum.calculate_property(mask1,
                                                      image.get_channel(0)) == 30 * 60 * 60 * 50 + 20 * 40 * 40 * 20
         assert PixelBrightnessSum.calculate_property(mask2, image.get_channel(0)) == 20 * 40 * 40 * 70
+        assert PixelBrightnessSum.calculate_property(mask3, image.get_channel(0)) == (30 * 60 * 60 - 20 * 40 * 40) * 50
 
 
 class TestVolume(object):
@@ -39,8 +45,12 @@ class TestVolume(object):
         image = get_cube_image()
         mask1 = image.get_channel(0) > 40
         mask2 = image.get_channel(0) > 60
+        mask3 = mask1 * ~mask2
         assert Volume.calculate_property(mask1, image.spacing, 1) == (100 * 30) * (50 * 60) * (50 * 60)
         assert Volume.calculate_property(mask2, image.spacing, 1) == (100 * 20) * (50 * 40) * (50 * 40)
+        assert Volume.calculate_property(mask3, image.spacing, 1) == \
+            (100 * 30) * (50 * 60) * (50 * 60) - (100 * 20) * (50 * 40) * (50 * 40)
+
 
 class TestComponentsNumber(object):
     def test_cube(self):
@@ -51,3 +61,79 @@ class TestComponentsNumber(object):
         assert ComponentsNumber.calculate_property(mask2) == 1
         assert ComponentsNumber.calculate_property(image.get_channel(0)) == 2
 
+
+class TestMaximumPixelBrightness:
+    def test_cube(self):
+        image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = mask1 * ~mask2
+        assert MaximumPixelBrightness.calculate_property(mask1, image.get_channel(0)) == 70
+        assert MaximumPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
+        assert MaximumPixelBrightness.calculate_property(mask3, image.get_channel(0)) == 50
+
+
+class TestMinimumPixelBrightness:
+    def test_cube(self):
+        image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        assert MinimumPixelBrightness.calculate_property(mask1, image.get_channel(0)) == 50
+        assert MinimumPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
+        assert MinimumPixelBrightness.calculate_property(mask3, image.get_channel(0)) == 0
+
+
+class TestMedianPixelBrightness:
+    def test_cube(self):
+        image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        assert MedianPixelBrightness.calculate_property(mask1, image.get_channel(0)) == 50
+        assert MedianPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
+        assert MedianPixelBrightness.calculate_property(mask3, image.get_channel(0)) == 0
+
+
+class TestMeanPixelBrightness:
+    def test_cube(self):
+        image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        assert MeanPixelBrightness.calculate_property(mask1, image.get_channel(0)) == \
+            (30 * 60 * 60 * 50 + 20 * 40 * 40 * 20) / (30 * 60 * 60)
+        assert MeanPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
+        assert MeanPixelBrightness.calculate_property(mask3, image.get_channel(0)) == \
+            (30 * 60 * 60 * 50 + 20 * 40 * 40 * 20) / (50 * 100 * 100)
+
+
+class TestStandardDeviationOfPixelBrightness:
+    def test_cube(self):
+        image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        mean = (30 * 60 * 60 * 50 + 20 * 40 * 40 * 20) / (30 * 60 * 60)
+        assert StandardDeviationOfPixelBrightness.calculate_property(mask1, image.get_channel(0)) == \
+            np.sqrt(((30 * 60 * 60 - 20 * 40 * 40) * (50 - mean) ** 2 + ((20 * 40 * 40) * (70 - mean) ** 2)) / (
+                    30 * 60 * 60))
+
+        assert StandardDeviationOfPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 0
+        mean = (30 * 60 * 60 * 50 + 20 * 40 * 40 * 20) / (50 * 100 * 100)
+        assert StandardDeviationOfPixelBrightness.calculate_property(mask3, image.get_channel(0)) == \
+            np.sqrt(((30 * 60 * 60 - 20 * 40 * 40) * (50 - mean) ** 2 + ((20 * 40 * 40) * (70 - mean) ** 2) +
+                    (50 * 100 * 100 - 30 * 60 * 60) * mean ** 2) / (50 * 100 * 100))
+
+
+class TestMomentOfInertia:
+    def test_cube(self):
+        image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        in1 = MomentOfInertia.calculate_property(mask1, image.get_channel(0), image.spacing)
+        in2 = MomentOfInertia.calculate_property(mask2, image.get_channel(0), image.spacing)
+        in3 = MomentOfInertia.calculate_property(mask3, image.get_channel(0), image.spacing)
+        assert in1 == in3
+        assert in1 > in2
