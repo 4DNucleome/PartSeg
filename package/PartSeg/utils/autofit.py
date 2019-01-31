add 2d statistics tests. fix momentum calculation

package/PartSeg/utils/autofit.py
@@ -77,20 +77,26 @@ def density_mass_center(image, voxel_size=(1.0, 1.0, 1.0)):
         :return np.ndarray
 
     """
-    x_size, y_size, z_size = image.shape
+    import itertools
+    single_dim = tuple([i for i, x in enumerate(image.shape) if x == 1])
+    iter_dim = [i for i, x in enumerate(image.shape) if x > 1]
+    elements = image.ndim - len(single_dim)
+    res = []
     denominator = float(np.sum(image))
-    m_x = np.sum(np.sum(image, axis=(1, 2)) * np.arange(x_size))
-    m_y = np.sum(np.sum(image, axis=(0, 2)) * np.arange(y_size))
-    m_z = np.sum(np.sum(image, axis=(0, 1)) * np.arange(z_size))
-    return np.array([m_x/denominator, m_y/denominator, m_z/denominator]) * voxel_size
+    for ax in itertools.combinations(iter_dim, elements):
+        item = ax[0]
+        ax = single_dim + ax[1:]
+        m = np.sum(np.sum(image, axis=ax) * np.arange(image.shape[item]))
+        res.append(m/denominator)
+    return np.array(res) * voxel_size
 
 
-def calculate_density_momentum(image, voxel_size=np.array([1., 1., 1.]), mass_center=None):
+def calculate_density_momentum(image: np.ndarray, voxel_size=np.array([1., 1., 1.]), mass_center=None):
     """Calculates image momentum."""
     if not mass_center:
         mass_center = density_mass_center(image, voxel_size)
     mass_center = np.array(mass_center)
-    points = np.transpose(np.nonzero(np.zeros(image.shape, dtype=np.uint8)+1))
+    points = np.transpose(np.nonzero(np.zeros(image.squeeze().shape, dtype=np.uint8)+1))
     points = points * voxel_size
     weights = np.squeeze(sp.cdist(points, np.array([mass_center]))**2)
     momentum = float(np.sum(weights * image.flatten()))

package/tests/test_statistics.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from math import isclose
 from PartSeg.tiff_image import Image
 from PartSeg.utils.analysis.statistics_calculation import Diameter, PixelBrightnessSum, Volume, ComponentsNumber, \
     MaximumPixelBrightness, MinimumPixelBrightness, MeanPixelBrightness, MedianPixelBrightness, \
@@ -15,6 +15,10 @@ def get_cube_array():
 
 def get_cube_image():
     return Image(get_cube_array(), (100, 50, 50), "")
+
+
+def get_square_image():
+    return Image(get_cube_array()[:, 25:26], (100, 50, 50), "")
 
 
 class TestDiameter(object):
@@ -39,6 +43,16 @@ class TestPixelBrightnessSum(object):
         assert PixelBrightnessSum.calculate_property(mask2, image.get_channel(0)) == 20 * 40 * 40 * 70
         assert PixelBrightnessSum.calculate_property(mask3, image.get_channel(0)) == (30 * 60 * 60 - 20 * 40 * 40) * 50
 
+    def test_square(self):
+        image = get_square_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = mask1 * ~mask2
+        assert PixelBrightnessSum.calculate_property(mask1, image.get_channel(0)) == 60 * 60 * 50 + 40 * 40 * 20
+        assert PixelBrightnessSum.calculate_property(mask2, image.get_channel(0)) == 40 * 40 * 70
+        assert PixelBrightnessSum.calculate_property(mask3, image.get_channel(0)) == (60 * 60 - 40 * 40) * 50
+
+
 
 class TestVolume(object):
     def test_cube(self):
@@ -51,6 +65,15 @@ class TestVolume(object):
         assert Volume.calculate_property(mask3, image.spacing, 1) == \
             (100 * 30) * (50 * 60) * (50 * 60) - (100 * 20) * (50 * 40) * (50 * 40)
 
+    def test_square(self):
+        image = get_square_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = mask1 * ~mask2
+        assert Volume.calculate_property(mask1, image.spacing, 1) == (50 * 60) * (50 * 60)
+        assert Volume.calculate_property(mask2, image.spacing, 1) == (50 * 40) * (50 * 40)
+        assert Volume.calculate_property(mask3, image.spacing, 1) == (50 * 60) * (50 * 60) - (50 * 40) * (50 * 40)
+
 
 class TestComponentsNumber(object):
     def test_cube(self):
@@ -61,10 +84,27 @@ class TestComponentsNumber(object):
         assert ComponentsNumber.calculate_property(mask2) == 1
         assert ComponentsNumber.calculate_property(image.get_channel(0)) == 2
 
+    def test_square(self):
+        image = get_square_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        assert ComponentsNumber.calculate_property(mask1) == 1
+        assert ComponentsNumber.calculate_property(mask2) == 1
+        assert ComponentsNumber.calculate_property(image.get_channel(0)) == 2
+
 
 class TestMaximumPixelBrightness:
     def test_cube(self):
         image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = mask1 * ~mask2
+        assert MaximumPixelBrightness.calculate_property(mask1, image.get_channel(0)) == 70
+        assert MaximumPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
+        assert MaximumPixelBrightness.calculate_property(mask3, image.get_channel(0)) == 50
+
+    def test_square(self):
+        image = get_square_image()
         mask1 = image.get_channel(0) > 40
         mask2 = image.get_channel(0) > 60
         mask3 = mask1 * ~mask2
@@ -83,10 +123,28 @@ class TestMinimumPixelBrightness:
         assert MinimumPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
         assert MinimumPixelBrightness.calculate_property(mask3, image.get_channel(0)) == 0
 
+    def test_square(self):
+        image = get_square_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        assert MinimumPixelBrightness.calculate_property(mask1, image.get_channel(0)) == 50
+        assert MinimumPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
+        assert MinimumPixelBrightness.calculate_property(mask3, image.get_channel(0)) == 0
+
 
 class TestMedianPixelBrightness:
     def test_cube(self):
         image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        assert MedianPixelBrightness.calculate_property(mask1, image.get_channel(0)) == 50
+        assert MedianPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
+        assert MedianPixelBrightness.calculate_property(mask3, image.get_channel(0)) == 0
+
+    def test_square(self):
+        image = get_square_image()
         mask1 = image.get_channel(0) > 40
         mask2 = image.get_channel(0) > 60
         mask3 = image.get_channel(0) >= 0
@@ -107,6 +165,17 @@ class TestMeanPixelBrightness:
         assert MeanPixelBrightness.calculate_property(mask3, image.get_channel(0)) == \
             (30 * 60 * 60 * 50 + 20 * 40 * 40 * 20) / (50 * 100 * 100)
 
+    def test_square(self):
+        image = get_square_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        assert MeanPixelBrightness.calculate_property(mask1, image.get_channel(0)) == \
+               (60 * 60 * 50 + 40 * 40 * 20) / (60 * 60)
+        assert MeanPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 70
+        assert MeanPixelBrightness.calculate_property(mask3, image.get_channel(0)) == \
+            (60 * 60 * 50 + 40 * 40 * 20) / (100 * 100)
+
 
 class TestStandardDeviationOfPixelBrightness:
     def test_cube(self):
@@ -121,14 +190,39 @@ class TestStandardDeviationOfPixelBrightness:
 
         assert StandardDeviationOfPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 0
         mean = (30 * 60 * 60 * 50 + 20 * 40 * 40 * 20) / (50 * 100 * 100)
-        assert StandardDeviationOfPixelBrightness.calculate_property(mask3, image.get_channel(0)) == \
+        assert isclose(StandardDeviationOfPixelBrightness.calculate_property(mask3, image.get_channel(0)),
             np.sqrt(((30 * 60 * 60 - 20 * 40 * 40) * (50 - mean) ** 2 + ((20 * 40 * 40) * (70 - mean) ** 2) +
-                    (50 * 100 * 100 - 30 * 60 * 60) * mean ** 2) / (50 * 100 * 100))
+                    (50 * 100 * 100 - 30 * 60 * 60) * mean ** 2) / (50 * 100 * 100)))
 
+    def test_square(self):
+        image = get_square_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        mean = (60 * 60 * 50 + 40 * 40 * 20) / (60 * 60)
+        assert StandardDeviationOfPixelBrightness.calculate_property(mask1, image.get_channel(0)) == \
+            np.sqrt(((60 * 60 - 40 * 40) * (50 - mean) ** 2 + ((40 * 40) * (70 - mean) ** 2)) / (60 * 60))
+
+        assert StandardDeviationOfPixelBrightness.calculate_property(mask2, image.get_channel(0)) == 0
+        mean = (60 * 60 * 50 + 40 * 40 * 20) / (100 * 100)
+        assert isclose(StandardDeviationOfPixelBrightness.calculate_property(mask3, image.get_channel(0)),
+            np.sqrt(((60 * 60 - 40 * 40) * (50 - mean) ** 2 + ((40 * 40) * (70 - mean) ** 2) +
+                    (100 * 100 - 60 * 60) * mean ** 2) / (100 * 100)))
 
 class TestMomentOfInertia:
     def test_cube(self):
         image = get_cube_image()
+        mask1 = image.get_channel(0) > 40
+        mask2 = image.get_channel(0) > 60
+        mask3 = image.get_channel(0) >= 0
+        in1 = MomentOfInertia.calculate_property(mask1, image.get_channel(0), image.spacing)
+        in2 = MomentOfInertia.calculate_property(mask2, image.get_channel(0), image.spacing)
+        in3 = MomentOfInertia.calculate_property(mask3, image.get_channel(0), image.spacing)
+        assert in1 == in3
+        assert in1 > in2
+
+    def test_square(self):
+        image = get_square_image()
         mask1 = image.get_channel(0) > 40
         mask2 = image.get_channel(0) > 60
         mask3 = image.get_channel(0) >= 0