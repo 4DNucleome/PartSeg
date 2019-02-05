bafe0ce57868735a945f66335a6f77086ff40bfc
Grzegorz Bokota <bokota+github@gmail.com>
2019-02-05 14:15:22 +0100

    move batch backed to utils

--- package/PartSeg/utils/analysis/batch_processing/batch_backend.py
+++ package/PartSeg/utils/analysis/batch_processing/batch_backend.py
@@ -18,9 +18,9 @@ from PartSeg.utils.analysis.io_utils import ProjectTuple
 from PartSeg.utils.analysis.load_functions import load_project
 from PartSeg.utils.analysis.analysis_utils import HistoryElement
 from PartSeg.utils.analysis.save_register import save_dict
-from ...utils.mask_create import calculate_mask
-from ...utils.segmentation.algorithm_base import report_empty_fun
-from ...utils.universal_const import UNIT_SCALE
+from PartSeg.utils.mask_create import calculate_mask
+from PartSeg.utils.segmentation.algorithm_base import report_empty_fun
+from PartSeg.utils.universal_const import UNIT_SCALE
 from PartSeg.tiff_image import ImageReader, Image
 
 
