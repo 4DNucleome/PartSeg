82d03423315c5a7aa82412ff742d602589d3fe67
Grzegorz Bokota <bokota+github@gmail.com>
2019-04-01 18:23:47 +0200

    code refactor to allow change image during segmentation

--- package/PartSeg/segmentation_mask/stack_settings.py
+++ package/PartSeg/segmentation_mask/stack_settings.py
@@ -1,6 +1,6 @@
 import typing
 from collections import defaultdict
-from copy import copy
+from copy import copy, deepcopy
 from typing import List
 import numpy as np
 from os import path
@@ -10,6 +10,7 @@ from qtpy.QtCore import Signal, Slot
 
 from PartSeg.tiff_image import Image
 from PartSeg.utils.algorithm_describe_base import SegmentationProfile
+from PartSeg.utils.segmentation.algorithm_base import SegmentationResult
 from ..project_utils_qt.settings import BaseSettings
 from PartSeg.utils.mask.io_functions import load_stack_segmentation, save_components, \
     SegmentationTuple
@@ -90,29 +91,43 @@ class StackSettings(BaseSettings):
                 (self.image.file_path != data.image.file_path or self.image.shape != data.image.shape):
             self.image = data.image
         self.blockSignals(signals)
-        self.set_segmentation(data.segmentation, self.keep_chosen_components, data.chosen_components,
-                              data.segmentation_parameters)
+        state = self.get_project_info()
+        # TODO Remove reperition this and set_segmentation code
+        if self.keep_chosen_components:
+            state2 = self.transform_state(state, data.segmentation, data.segmentation_parameters, data.chosen_components,
+                                          self.keep_chosen_components)
+            self.chosen_components_widget.set_chose(list(sorted(state2.segmentation_parameters.keys())),
+                                                    state2.chosen_components)
+            self.segmentation = state2.segmentation
+            self.components_parameters_dict = state2.segmentation_parameters
+        else:
+            self.chosen_components_widget.set_chose(list(sorted(data.segmentation_parameters.keys())),
+                                                    data.chosen_components)
+            self.segmentation = data.segmentation
+            self.components_parameters_dict = data.segmentation_parameters
 
-    def set_segmentation(self, new_segmentation_data, save_chosen=True, list_of_components=None,
-                         segmentation_parameters=None):
+    @staticmethod
+    def transform_state(state: SegmentationTuple, new_segmentation_data: np.ndarray, segmentation_parameters: typing.Dict,
+                        list_of_components: List[int],
+                        save_chosen: bool=True) -> SegmentationTuple:
+
         if list_of_components is None:
             list_of_components = []
         if segmentation_parameters is None:
             segmentation_parameters = defaultdict(lambda: None)
-        if save_chosen and self.segmentation is not None:
-            segmentation = np.zeros(self.segmentation.shape, dtype=self.segmentation.dtype)
+        if save_chosen and state.segmentation is not None:
+            segmentation = np.zeros(state.segmentation.shape, dtype=state.segmentation.dtype)
             components_parameters_dict = {}
-            for i, val in enumerate(sorted(self.chosen_components_widget.get_chosen()), 1):
-                segmentation[self.segmentation == val] = i
-                components_parameters_dict[i] = self.components_parameters_dict[val]
-            base_chose = list(range(1, len(self.chosen_components_widget.get_chosen()) + 1))
-            self.components_parameters_dict = components_parameters_dict
+            for i, val in enumerate(sorted(state.chosen_components), 1):
+                segmentation[state.segmentation == val] = i
+                components_parameters_dict[i] = state.segmentation_parameters[val]
+            base_chose = list(range(1, len(state.chosen_components) + 1))
         else:
             segmentation = None
             base_chose = []
-            self.components_parameters_dict = {}
+            components_parameters_dict = {}
         if new_segmentation_data is not None:
-            self.image.fit_array_to_image(new_segmentation_data)
+            state.image.fit_array_to_image(new_segmentation_data)
             num = new_segmentation_data.max()
             if segmentation is not None:
                 new_segmentation = np.copy(new_segmentation_data)
@@ -127,18 +142,40 @@ class StackSettings(BaseSettings):
                         if i in list_of_components:
                             chosen_components.append(base_index)
                         components_list.append(base_index)
-                        self.components_parameters_dict[base_index] = segmentation_parameters[i]
+                        components_parameters_dict[base_index] = segmentation_parameters[i]
                         base_index += 1
-                self.chosen_components_widget.set_chose(components_list, chosen_components)
-                self.segmentation = segmentation
+                return state._replace(segmentation=segmentation, chosen_components=chosen_components,
+                                      segmentation_parameters=components_parameters_dict)
             else:
-                self.chosen_components_widget.set_chose(range(1, num + 1), list_of_components)
-                self.segmentation = new_segmentation_data
-                for i in range(1, num+1):
-                    self.components_parameters_dict[i] = segmentation_parameters[i]
+                for i in range(1, num + 1):
+                    components_parameters_dict[i] = segmentation_parameters[i]
+                return state._replace(segmentation=new_segmentation_data, chosen_components=list_of_components,
+                                      segmentation_parameters=components_parameters_dict)
         else:
-            self.chosen_components_widget.set_chose(base_chose, base_chose)
-            self.segmentation = segmentation
+            return state._replace(segmentation=segmentation, chosen_components=base_chose,
+                                  segmentation_parameters=components_parameters_dict)
+
+    def set_segmentation(self, new_segmentation_data, save_chosen=True, list_of_components=None,
+                         segmentation_parameters=None):
+        if list_of_components is None:
+            list_of_components = []
+        if segmentation_parameters is None:
+            segmentation_parameters = defaultdict(lambda: None)
+        state = self.get_project_info()
+        if save_chosen:
+            state2 = self.transform_state(state, new_segmentation_data, segmentation_parameters,
+                                          list_of_components,
+                                          save_chosen)
+            self.chosen_components_widget.set_chose(list(sorted(state2.segmentation_parameters.keys())),
+                                                    state2.chosen_components)
+            self.segmentation = state2.segmentation
+            self.components_parameters_dict = state2.segmentation_parameters
+        else:
+            self.chosen_components_widget.set_chose(list(sorted(segmentation_parameters.keys())),
+                                                    list_of_components)
+            self.segmentation = new_segmentation_data
+            self.components_parameters_dict = segmentation_parameters
+
 
     @staticmethod
     def verify_image(image: Image, silent=True) -> typing.Union[Image, bool]:
