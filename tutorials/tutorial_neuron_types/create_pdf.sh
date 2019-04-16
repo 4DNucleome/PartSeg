ed2f009a2cb5a2e4b74741a52adefccc1fa9a72c
Grzegorz Bokota <bokota+github@gmail.com>
2019-04-16 16:56:13 +0200

    update tutorial notebook

---
+++ tutorials/tutorial_neuron_types/create_pdf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+jupyter nbconvert --to latex Neuron_types_example.ipynb
+sed -r -i 's/documentclass\[11pt\]\{article\}/documentclass[8pt]{extarticle}/' Neuron_types_example.tex
+sed -r -i 's/geometry\{verbose,tmargin=1in,bmargin=1in,lmargin=1in,rmargin=1in}/geometry{verbose,tmargin=0.5in,bmargin=0.5in,lmargin=0.2in,rmargin=0.2in}/' Neuron_types_example.tex
+pdflatex Neuron_types_example.tex
