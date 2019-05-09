0151a1ddfc7f6cc0c724e86809b2666824a6b588
Grzegorz Bokota <bokota+github@gmail.com>
2019-05-09 17:28:33 +0200

    add possibility of finding plugins with entry point

--- package/PartSeg/plugins/__init__.py
+++ package/PartSeg/plugins/__init__.py
@@ -1,21 +1,20 @@
 import importlib
 import pkgutil
+import pkg_resources
 
 
 def get_plugins():
-    packages = pkgutil.iter_modules(__path__)
-    return list(packages)
+    packages = pkgutil.iter_modules(__path__, __name__ + ".")
+    packages2 = pkg_resources.iter_entry_points("PartSeg.plugins")
+    return [importlib.import_module(el.name) for el in packages] + [el.load() for el in packages2]
 
 
-plugins_loaded = False
+plugins_loaded = set()
 
 
 def register():
-    global plugins_loaded
-    if plugins_loaded:
-        return
-    plugins_loaded = True
-
-    print("plugins load", get_plugins())
+    # print("plugins load", get_plugins())
     for el in get_plugins():
-        importlib.import_module("." + el.name, __package__).register()
+        if hasattr(el, "register") and el.__name__ not in plugins_loaded:
+            el.register()
+            plugins_loaded.add(el.__name__)
