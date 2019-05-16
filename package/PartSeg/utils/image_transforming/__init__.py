9cfe68c2cac79e64e70bf8404cba9086903b97a1
Grzegorz Bokota <bokota+github@gmail.com>
2019-05-16 17:31:05 +0200

    add swap time/z as image operations

--- package/PartSeg/utils/image_transforming/__init__.py
+++ package/PartSeg/utils/image_transforming/__init__.py
@@ -1,6 +1,6 @@
 from ..algorithm_describe_base import Register
 from .interpolate_image import InterpolateImage
 from .transform_base import TransformBase
+from .swap_time_stack import SwapTimeStack
 
-
-image_transform_dict = Register(InterpolateImage)
+image_transform_dict = Register(InterpolateImage, SwapTimeStack)
---
+++ package/PartSeg/utils/image_transforming/swap_time_stack.py
@@ -0,0 +1,29 @@
+import typing
+from typing import List, Optional, Callable
+
+from PartSeg.tiff_image import Image
+from PartSeg.utils.algorithm_describe_base import AlgorithmProperty
+from .transform_base import TransformBase
+
+
+class SwapTimeStack(TransformBase):
+    @classmethod
+    def transform(self, image: Image, arguments: dict,
+                  callback_function: Optional[Callable[[str, int], None]] = None) -> Image:
+        return image.swap_time_and_stack()
+
+    @classmethod
+    def get_fields_per_dimension(cls, component_list: List[str]):
+        return cls.get_fields()
+
+    @classmethod
+    def calculate_initial(cls, image: Image):
+        return cls.get_default_values()
+
+    @classmethod
+    def get_name(cls) -> str:
+        return "Swap time and Z dim"
+
+    @classmethod
+    def get_fields(cls) -> typing.List[typing.Union[AlgorithmProperty, str]]:
+        return []
