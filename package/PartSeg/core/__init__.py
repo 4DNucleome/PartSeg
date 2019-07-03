286839d9151a23ad28bb046d8bd4ee87336a678d
Grzegorz Bokota <bokota+github@gmail.com>
2019-07-03 16:30:58 +0200

    code for aliasing modules

---
+++ package/PartSeg/core/__init__.py
@@ -0,0 +1,30 @@
+import sys
+import os
+from importlib.abc import MetaPathFinder, Loader
+import importlib
+# noinspection PyUnresolvedReferences
+from PartSeg.utils import *
+
+
+class MyLoader(Loader):
+    def module_repr(self, module):
+        return repr(module)
+
+    def load_module(self, fullname):
+        old_name = fullname
+        names = fullname.split(".")
+        names[1] = "utils"
+        fullname = ".".join(names)
+        module = importlib.import_module(fullname)
+        sys.modules[old_name] = module
+        return module
+        
+
+class MyImport(MetaPathFinder):
+    def find_module(self, fullname, path=None):
+        names = fullname.split(".")
+        if len(names) >= 2 and names[0] == "PartSeg" and names[1] == "core":
+            return MyLoader()
+
+
+sys.meta_path.append(MyImport())
