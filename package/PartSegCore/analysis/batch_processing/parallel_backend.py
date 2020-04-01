08b9b924b1cc4b0a01908746ced0a5042fde72ee
Grzegorz Bokota <bokota+github@gmail.com>
2020-04-01 20:09:14 +0200

    fix PARTSEG-3A

--- package/PartSegCore/analysis/batch_processing/parallel_backend.py
+++ package/PartSegCore/analysis/batch_processing/parallel_backend.py
@@ -214,7 +214,7 @@ class BatchWorker:
             exc_type, _exc_obj, exc_tb = sys.exc_info()
             f_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, f_name, exc_tb.tb_lineno, file=sys.stderr)
-            self.result_queue.put((task_uuid, [(e, traceback.extract_tb(e.__traceback__))]))
+            self.result_queue.put((task_uuid, (e, traceback.extract_tb(e.__traceback__))))
 
     def run(self):
         """Worker main loop"""
