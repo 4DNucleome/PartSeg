4f80e4ecd0a81056719c91a7cfc19fdfe294eceb
Grzegorz Bokota <bokota+github@gmail.com>
2020-09-03 16:40:09 +0200

    add script to create minimal requirements base on setup.cfg

---
+++ .travis/create_minimal_req.py
@@ -0,0 +1,11 @@
+import os
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), "..", "setup.cfg"))
+
+minimum_requirements = config["options"]["install_requires"].replace(">=", "==")
+
+with open(os.path.join(os.path.dirname(__file__), "minimal-req.txt"), "w") as f_p:
+    f_p.write(minimum_requirements)
+    f_p.write("\n")
