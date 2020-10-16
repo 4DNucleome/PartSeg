2959e9bd0eb71ee3e8dc16ca1a638ef57fc9f763
Grzegorz Bokota <bokota+github@gmail.com>
2020-10-17 01:07:36 +0200

    add tests for Properties Widget

--- package/tests/test_PartSeg/conftest.py
+++ package/tests/test_PartSeg/conftest.py
@@ -1,5 +1,9 @@
 import pytest
 
+from PartSegCore.algorithm_describe_base import ROIExtractionProfile
+from PartSegCore.analysis import SegmentationPipeline, SegmentationPipelineElement
+from PartSegCore.segmentation.restartable_segmentation_algorithms import BorderRim, LowerThresholdAlgorithm
+
 try:
     # to ignore problem with test in docker container
 
@@ -29,6 +33,22 @@ try:
         settings.image = image
         settings.set_project_info(analysis_segmentation2)
         return settings
+
+    @pytest.fixture
+    def border_rim_profile():
+        return ROIExtractionProfile("border_profile", BorderRim.get_name(), BorderRim.get_default_values())
+
+    @pytest.fixture
+    def lower_threshold_profile():
+        return ROIExtractionProfile(
+            "lower_profile", LowerThresholdAlgorithm.get_name(), LowerThresholdAlgorithm.get_default_values()
+        )
+
+    @pytest.fixture
+    def sample_pipeline(border_rim_profile, lower_threshold_profile, mask_property):
+        return SegmentationPipeline(
+            "sample_pipeline", border_rim_profile, [SegmentationPipelineElement(lower_threshold_profile, mask_property)]
+        )
 
 
 except (RuntimeError, ImportError):
