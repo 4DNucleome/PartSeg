83bb226f38669b08653675fa080845a655f32118
Grzegorz Bokota <bokota+github@gmail.com>
2020-10-19 01:12:55 +0200

    fix for old Qt version

--- package/PartSeg/common_gui/searchable_combo_box.py
+++ package/PartSeg/common_gui/searchable_combo_box.py
@@ -1,4 +1,6 @@
-from qtpy.QtCore import Qt
+from packaging.version import parse
+from qtpy import QT_VERSION
+from qtpy.QtCore import Qt, Signal
 from qtpy.QtWidgets import QComboBox, QCompleter
 
 
@@ -7,12 +9,21 @@ class SearchCombBox(QComboBox):
     ComboCox with completer for fast search in multiple options
     """
 
+    if parse(QT_VERSION) < parse("5.14.0"):
+        textActivated = Signal(str)
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setEditable(True)
         self.completer_object = QCompleter()
         self.completer_object.setCaseSensitivity(Qt.CaseInsensitive)
         self.setCompleter(self.completer_object)
+        # FIXME
+        if parse(QT_VERSION) < parse("5.14.0"):
+            self.currentIndexChanged.connect(self._text_activated)
+
+    def _text_activated(self):
+        self.textActivated.emmit(self.currentText())
 
     def addItem(self, *args):
         super().addItem(*args)
