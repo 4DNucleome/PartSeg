d4eaa7435a1e44ea226fccfba767a3bcc71eac4f
Grzegorz Bokota <bokota+github@gmail.com>
2021-02-20 21:12:07 +0100

    initial widget

--- package/PartSeg/common_gui/napari_viewer_wrap.py
+++ package/PartSeg/common_gui/napari_viewer_wrap.py
@@ -1,5 +1,16 @@
+from typing import Optional
+
 from napari import Viewer as NViewer
+from qtpy.QtWidgets import QCheckBox
+
+from PartSeg.common_backend.base_settings import BaseSettings
 
 
 class Viewer(NViewer):
     _napari_app_id = False
+
+    def __init__(self, settings: Optional[BaseSettings] = None, **kwargs):
+        super().__init__(**kwargs)
+        self.settings = settings
+        self.sync_chk = QCheckBox("Enable layer sync")
+        self.window.add_dock_widget(self.sync_chk, area="left")
