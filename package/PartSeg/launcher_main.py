492f5ca5c22cb605f3798f6adb6cc222cf3dedea
Grzegorz Bokota <bokota+github@gmail.com>
2021-03-02 01:32:33 +0100

    fixes for napari 0.4.6

--- package/PartSeg/launcher_main.py
+++ package/PartSeg/launcher_main.py
@@ -5,11 +5,7 @@ import os
 import sys
 from functools import partial
 
-try:
-    from napari._qt.qthreading import wait_for_workers_to_quit
-except ImportError:
-    from napari._qt.threading import wait_for_workers_to_quit
-
+from napari.qt import get_app
 from qtpy.QtCore import Qt
 from qtpy.QtGui import QFontDatabase
 
@@ -78,7 +74,7 @@ def main():
     CustomApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     my_app = CustomApplication(sys.argv, name="PartSeg", icon=os.path.join(icons_dir, "icon.png"))
     my_app.check_release()
-    my_app.aboutToQuit.connect(wait_for_workers_to_quit)
+    get_app()
     QFontDatabase.addApplicationFont(os.path.join(font_dir, "Symbola.ttf"))
     if args.gui == "roi_analysis" or args.mf:
         from PartSeg import plugins
