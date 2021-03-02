e4565a6f20c715173e60e2316108d5cac7bf53c8
Grzegorz Bokota <bokota+github@gmail.com>
2021-03-02 02:14:08 +0100

    backward compatybility

--- package/PartSeg/launcher_main.py
+++ package/PartSeg/launcher_main.py
@@ -5,7 +5,6 @@ import os
 import sys
 from functools import partial
 
-from napari.qt import get_app
 from qtpy.QtCore import Qt
 from qtpy.QtGui import QFontDatabase
 
@@ -74,7 +73,12 @@ def main():
     CustomApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
     my_app = CustomApplication(sys.argv, name="PartSeg", icon=os.path.join(icons_dir, "icon.png"))
     my_app.check_release()
-    get_app()
+    try:
+        from napari.qt import get_app
+
+        get_app()
+    except ImportError:
+        pass
     QFontDatabase.addApplicationFont(os.path.join(font_dir, "Symbola.ttf"))
     if args.gui == "roi_analysis" or args.mf:
         from PartSeg import plugins
