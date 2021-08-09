7d78d273b89228334f8ffdf10856eefa8e634f8d
Grzegorz Bokota <bokota+github@gmail.com>
2021-08-09 13:10:17 +0200

    fix paths

--- build_utils/create_and_pack_executable.py
+++ build_utils/create_and_pack_executable.py
@@ -1,9 +1,7 @@
-import codecs
+import argparse
 import os
 import platform
-import re
 import shutil
-import sys
 import tarfile
 import zipfile
 
@@ -11,55 +9,61 @@ from PyInstaller.__main__ import run as pyinstaller_run
 
 import PartSeg
 
-if len(sys.argv) == 2:
-    base_path = os.path.abspath(sys.argv[1])
-else:
-    base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# if len(sys.argv) == 2:
+#     base_path = os.path.abspath(sys.argv[1])
+# else:
+#     base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-os.chdir(base_path)
+parser = argparse.ArgumentParser("PartSeg build")
+parser.add_argument(
+    "--spec", default=os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "launcher.spec")
+)
+parser.add_argument("--working-dir", default=os.path.abspath(os.curdir))
 
-pyinstaller_run(["-y", "--debug=all", "launcher.spec"])
-
-
-def read(*parts):
-    with codecs.open(os.path.join(base_path, *parts), "r") as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^version = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+args = parser.parse_args()
 
 
-version = find_version(os.path.join(os.path.dirname(PartSeg.__file__), "version.py"))
+pyinstaller_run(
+    [
+        "-y",
+        "--debug=all",
+        args.spec,
+        "--distpath",
+        os.path.join(args.working_dir, "dist"),
+        "--workpath",
+        os.path.join(args.working_dir, "dist"),
+    ]
+)
+
 
 name_dict = {"Linux": "linux", "Windows": "windows", "Darwin": "macos"}
 
 system_name = name_dict[platform.system()]
 
-os.makedirs(os.path.join(base_path, "dist2"), exist_ok=True)
+os.makedirs(os.path.join(args.working_dir, "dist2"), exist_ok=True)
 
 if platform.system() == "Darwin":
-    arch_file = tarfile.open(os.path.join(base_path, "dist2", f"PartSeg-{version}-{system_name}.tgz"), "w:gz")
+    arch_file = tarfile.open(
+        os.path.join(args.working_dir, "dist2", f"PartSeg-{PartSeg.__version__}-{system_name}.tgz"), "w:gz"
+    )
     arch_file.write = arch_file.add
 else:
     arch_file = zipfile.ZipFile(
-        os.path.join(base_path, "dist2", f"PartSeg-{version}-{system_name}.zip"), "w", zipfile.ZIP_DEFLATED
+        os.path.join(args.working_dir, "dist2", f"PartSeg-{PartSeg.__version__}-{system_name}.zip"),
+        "w",
+        zipfile.ZIP_DEFLATED,
     )
-base_zip_path = os.path.join(base_path, "dist")
+base_zip_path = os.path.join(args.working_dir, "dist")
 
 dir_name = "PartSeg.app" if platform.system() == "Darwin2" else "PartSeg"
 
-if platform.system() == "Darwin" and os.path.exists(os.path.join(base_path, "dist", dir_name, "PyQt5", "Qt")):
+if platform.system() == "Darwin" and os.path.exists(os.path.join(args.working_dir, "dist", dir_name, "PyQt5", "Qt")):
     shutil.move(
-        os.path.join(base_path, "dist", dir_name, "PyQt5", "Qt"),
-        os.path.join(base_path, "dist", dir_name, "PyQt5", "Qt5"),
+        os.path.join(args.working_dir, "dist", dir_name, "PyQt5", "Qt"),
+        os.path.join(args.working_dir, "dist", dir_name, "PyQt5", "Qt5"),
     )
 
-for root, dirs, files in os.walk(os.path.join(base_path, "dist", dir_name), topdown=False, followlinks=True):
+for root, dirs, files in os.walk(os.path.join(args.working_dir, "dist", dir_name), topdown=False, followlinks=True):
     for file_name in files:
         arch_file.write(os.path.join(root, file_name), os.path.relpath(os.path.join(root, file_name), base_zip_path))
 
