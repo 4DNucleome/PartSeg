dfc0acc50e81c118129a9347de58f3b3e8f78bb2
Grzegorz Bokota <bokota+github@gmail.com>
2021-08-20 22:32:27 +0200

    check solution

--- package/PartSeg/common_gui/select_multiple_files.py
+++ package/PartSeg/common_gui/select_multiple_files.py
@@ -145,7 +145,7 @@ class AddFiles(QWidget):
             event.acceptProposedAction()
 
     def dropEvent(self, event: QDropEvent):
-        files_list = event.mimeData().text().split()
+        files_list = event.mimeData().text().split("\n")
         self.parse_drop_file_list(files_list)
 
     def parse_drop_file_list(self, files_list):
@@ -155,7 +155,7 @@ class AddFiles(QWidget):
             if os.path.isabs(file_path):
                 res_list.append(file_path)
             else:
-                res_list.append(os.path.join(base_path, file_path))
+                res_list.append(os.path.join(base_path, file_path.strip()))
         missed_files = [x for x in res_list if not os.path.exists(x)]
         if missed_files:
             if len(missed_files) > 6:
