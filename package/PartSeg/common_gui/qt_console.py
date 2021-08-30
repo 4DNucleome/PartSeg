be70cc0fbaa635cbcf751080c9a9083243b83053
Grzegorz Bokota <bokota+github@gmail.com>
2021-08-30 19:21:16 +0200

    fix lgtm

--- package/PartSeg/common_gui/qt_console.py
+++ package/PartSeg/common_gui/qt_console.py
@@ -36,16 +36,14 @@ def str_to_rgb(arg):
 if sys.platform.startswith("win") and sys.version_info >= (3, 8):
     import asyncio
 
-    try:
-        from asyncio import WindowsProactorEventLoopPolicy, WindowsSelectorEventLoopPolicy
-    except ImportError:
-        pass
-        # not affected
-    else:
-        if type(asyncio.get_event_loop_policy()) is WindowsProactorEventLoopPolicy:
-            # WindowsProactorEventLoopPolicy is not compatible with tornado 6
-            # fallback to the pre-3.8 default of Selector
-            asyncio.set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+    if (
+        hasattr(asyncio, "WindowsProactorEventLoopPolicy")
+        and hasattr(asyncio, "WindowsSelectorEventLoopPolicy")
+        and type(asyncio.get_event_loop_policy()) is asyncio.WindowsProactorEventLoopPolicy
+    ):
+        # WindowsProactorEventLoopPolicy is not compatible with tornado 6
+        # fallback to the pre-3.8 default of Selector
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 class QtConsole(RichJupyterWidget):
