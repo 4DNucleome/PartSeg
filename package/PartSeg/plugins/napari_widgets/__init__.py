1b26164b23fcd9cd30731fba928b4b40957f27b0
Grzegorz Bokota <bokota+github@gmail.com>
2021-10-07 19:08:29 +0200

    fix failng tests

--- package/PartSeg/plugins/napari_widgets/__init__.py
+++ package/PartSeg/plugins/napari_widgets/__init__.py
@@ -1,11 +1,12 @@
 from napari_plugin_engine import napari_hook_implementation
 
-from PartSeg.plugins.napari_widgets.measurement_widget import SimpleMeasurement
 from PartSeg.plugins.napari_widgets.roi_extraction_algorithms import ROIAnalysisExtraction, ROIMaskExtraction
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
+    from PartSeg.plugins.napari_widgets.measurement_widget import SimpleMeasurement
+
     return SimpleMeasurement
 
 
