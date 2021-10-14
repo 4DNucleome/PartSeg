9bf7b7897f89df65dd3900e4f2d2bda26c16fd02
Grzegorz Bokota <bokota+github@gmail.com>
2021-10-14 11:56:39 +0200

    Fix pre release test failiture (#401)

--- package/tests/conftest.py
+++ package/tests/conftest.py
@@ -1,8 +1,11 @@
 import dataclasses
 import itertools
+import multiprocessing as mp
 import os
+import signal
 from copy import deepcopy
 from pathlib import Path
+from queue import Empty
 
 import numpy as np
 import pytest
@@ -28,6 +31,34 @@ def data_test_dir():
 def bundle_test_dir():
     """Return path to directory with test data"""
     return Path(os.path.join(os.path.dirname(__file__), "test_data"))
+
+
+def wait_sigint(q: mp.Queue, pid):
+    try:
+        q.get(timeout=20 * 60)
+    except Empty:
+        print("Timeout")
+        os.kill(pid, signal.SIGINT)
+        import time
+
+        time.sleep(30)
+        try:
+            os.kill(pid, 0)
+        except OSError:
+            pass
+        else:
+            os.kill(pid, signal.SIGKILL)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def sigint_after_time():
+    manager = mp.Manager()
+    q = manager.Queue()
+    p = mp.Process(target=wait_sigint, args=(q, os.getpid()))
+    p.start()
+    yield
+    q.put(1)
+    p.join()
 
 
 @pytest.fixture
