28b206c4b9450126bd227c941e09df681b3ac631
Grzegorz Bokota <bokota+github@gmail.com>
2021-10-25 21:45:53 +0200

    add tests for closing holes

--- package/tests/test_PartSegCore/segmentation/test_segmentation_algorithm.py
+++ package/tests/test_PartSegCore/segmentation/test_segmentation_algorithm.py
@@ -1,11 +1,16 @@
 from typing import Type
 
+import numpy as np
 import pytest
 
 from PartSegCore.segmentation import ROIExtractionAlgorithm
 from PartSegCore.segmentation.algorithm_base import ROIExtractionResult, SegmentationLimitException
 from PartSegCore.segmentation.restartable_segmentation_algorithms import final_algorithm_list as restartable_list
-from PartSegCore.segmentation.segmentation_algorithm import CellFromNucleusFlow, ThresholdFlowAlgorithm
+from PartSegCore.segmentation.segmentation_algorithm import (
+    CellFromNucleusFlow,
+    ThresholdFlowAlgorithm,
+    close_small_holes,
+)
 from PartSegCore.segmentation.segmentation_algorithm import final_algorithm_list as algorithm_list
 
 
@@ -39,6 +44,7 @@ def fix_threshold_flow(monkeypatch):
 def test_segmentation_algorithm(image, algorithm: Type[ROIExtractionAlgorithm], masking):
     assert algorithm.support_z() is True
     assert algorithm.support_time() is False
+    assert isinstance(algorithm.get_steps_num(), int)
     instance = algorithm()
     instance.set_image(image)
     if masking:
@@ -52,3 +58,14 @@ def test_segmentation_algorithm(image, algorithm: Type[ROIExtractionAlgorithm], 
         assert isinstance(instance.get_info_text(), str)
         assert isinstance(res, ROIExtractionResult)
     instance.clean()
+
+
+@pytest.mark.parametrize("ndim", (2, 3))
+@pytest.mark.parametrize("dtype", (np.uint8, bool))
+def test_close_small_holes(ndim, dtype):
+    data = np.zeros((10,) * ndim, dtype=dtype)
+    data[(slice(1, -1),) * ndim] = 1
+    copy = data.copy()
+    data[(slice(3, -3),) * ndim] = 0
+    res = close_small_holes(data, 5 ** 2)
+    assert np.all(res == copy)
