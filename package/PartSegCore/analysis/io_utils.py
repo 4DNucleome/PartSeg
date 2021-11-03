efa883e56461a02c6a28cc1bab6dd478d2bcc702
Grzegorz Bokota <bokota+github@gmail.com>
2021-11-03 23:04:05 +0100

    fix python 3.9.7 bug

--- package/PartSegCore/analysis/io_utils.py
+++ package/PartSegCore/analysis/io_utils.py
@@ -52,6 +52,15 @@ class ProjectTuple(ProjectInfoBase):
             f"history={repr(self.history)},\nalgorithm_parameters={self.algorithm_parameters},\nerrors={self.errors})"
         )
 
+    if sys.version_info[:3] == (3, 9, 7):
+
+        @property
+        def roi(self):
+            import warnings
+
+            warnings.warn("roi is deprecated", DeprecationWarning, 2)
+            return self.roi_info.roi
+
 
 class MaskInfo(typing.NamedTuple):
     """
