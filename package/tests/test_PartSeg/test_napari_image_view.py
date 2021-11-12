d92310cc7103f6cebdbb07a2bc0c4e234f1f0a53
Grzegorz Bokota <bokota+github@gmail.com>
2021-11-12 18:11:13 +0100

    test napar_image_view._print_dict

--- package/tests/test_PartSeg/test_napari_image_view.py
+++ package/tests/test_PartSeg/test_napari_image_view.py
@@ -1,7 +1,7 @@
 import numpy as np
 from napari.layers import Image as NapariImage
 
-from PartSeg.common_gui.napari_image_view import ImageInfo
+from PartSeg.common_gui.napari_image_view import ImageInfo, _print_dict
 from PartSegImage import Image
 
 
@@ -13,3 +13,15 @@ def test_image_info():
     image_info.layers.append(NapariImage(image_info.image.get_channel(0), scale=(1, 1, 10, 10)))
     assert image_info.coords_in([0.5, 0.5, 1, 1])
     assert np.all(image_info.translated_coords([1, 1, 1, 1]) == [1, 1, 1, 1])
+
+
+def test_print_dict():
+    dkt = {"a": 1, "b": {"e": 1, "d": [1, 2, 4]}}
+    res = _print_dict(dkt)
+    lines = res.split("\n")
+    assert len(lines) == 4
+    assert lines[0].startswith("a")
+    assert lines[2].startswith("  e")
+
+
+# class TestImageView()
