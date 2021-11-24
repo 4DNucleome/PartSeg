465daa79dae3c5c3d7b50cbb754d893378ade249
Grzegorz Bokota <bokota+github@gmail.com>
2021-11-24 11:56:03 +0100

    make test separated and skip them on windows

--- package/tests/test_PartSeg/test_napari_image_view.py
+++ package/tests/test_PartSeg/test_napari_image_view.py
@@ -1,10 +1,12 @@
 # pylint: disable=R0201
+import platform
 
 import numpy as np
 import pytest
 import qtpy
 from napari.layers import Image as NapariImage
 from qtpy.QtCore import QPoint
+from test_PartSeg.utils import CI_BUILD
 
 from PartSeg.common_gui.channel_control import ChannelProperty
 from PartSeg.common_gui.napari_image_view import ORDER_DICT, ImageInfo, ImageView, QMenu, _print_dict
@@ -154,6 +156,25 @@ class TestImageView:
         view.update_spacing_info()
         assert np.all(view.image_info[str(tmp_path / "test2.tiff")].mask.scale == (1, 10 ** 5, 10 ** 5, 10 ** 5))
 
+    @pytest.mark.skipif((platform.system() == "Windows") and CI_BUILD, reason="glBindFramebuffer with no OpenGL")
+    def test_mask_control_visibility(self, base_settings, image2, qtbot, tmp_path):
+        ch_prop = ChannelProperty(base_settings, "test")
+        view = ImageView(base_settings, channel_property=ch_prop, name="test")
+        qtbot.addWidget(ch_prop)
+        qtbot.addWidget(view)
+        base_settings.image = image2
+        view.show()
+        assert not view.mask_chk.isVisible()
+        view.set_mask()
+        assert not view.mask_chk.isVisible()
+        with qtbot.waitSignal(base_settings.mask_changed):
+            base_settings.mask = np.zeros(image2.get_channel(0).shape, dtype=np.uint8)
+        assert view.mask_chk.isVisible()
+        with qtbot.waitSignal(base_settings.mask_changed):
+            base_settings.mask = None
+        assert not view.mask_chk.isVisible()
+        view.hide()
+
     def test_points_rendering(self, base_settings, image2, qtbot, tmp_path):
         ch_prop = ChannelProperty(base_settings, "test")
         view = ImageView(base_settings, channel_property=ch_prop, name="test")
@@ -166,6 +187,21 @@ class TestImageView:
         assert view.points_layer.visible
         view.toggle_points_visibility()
         assert not view.points_layer.visible
+
+    @pytest.mark.skipif((platform.system() == "Windows") and CI_BUILD, reason="glBindFramebuffer with no OpenGL")
+    def test_points_button_visibility(self, base_settings, image2, qtbot, tmp_path):
+        ch_prop = ChannelProperty(base_settings, "test")
+        view = ImageView(base_settings, channel_property=ch_prop, name="test")
+        qtbot.addWidget(ch_prop)
+        qtbot.addWidget(view)
+        base_settings.image = image2
+        view.show()
+        assert not view.points_view_button.isVisible()
+        base_settings.points = [(0, 5, 5, 5)]
+        assert view.points_view_button.isVisible()
+        base_settings.points = None
+        assert not view.points_view_button.isVisible()
+        view.hide()
 
     @pyside_skip
     def test_dim_menu(self, base_settings, image2, qtbot, monkeypatch):
