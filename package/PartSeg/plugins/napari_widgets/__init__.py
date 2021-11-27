c0c267cee3d0b9a3d5af80b7510bd4f649c6b7d0
Grzegorz Bokota <bokota+github@gmail.com>
2021-11-27 16:38:12 +0100

    add search labels as napari widget

--- package/PartSeg/plugins/napari_widgets/__init__.py
+++ package/PartSeg/plugins/napari_widgets/__init__.py
@@ -2,6 +2,7 @@ from napari_plugin_engine import napari_hook_implementation
 
 from PartSeg.plugins.napari_widgets.mask_create_widget import MaskCreateNapari
 from PartSeg.plugins.napari_widgets.roi_extraction_algorithms import ROIAnalysisExtraction, ROIMaskExtraction
+from PartSeg.plugins.napari_widgets.search_label_widget import SearchLabel
 
 
 @napari_hook_implementation
@@ -31,3 +32,8 @@ def napari_experimental_provide_dock_widget4():
     from PartSeg.plugins.napari_widgets.measurement_widget import Measurement
 
     return Measurement
+
+
+@napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
+def napari_experimental_provide_dock_widget5():
+    return SearchLabel
