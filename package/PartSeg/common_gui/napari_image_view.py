744893e10e712726175962d73c43618731f1e47a
Grzegorz Bokota <bokota+github@gmail.com>
2021-12-07 12:34:29 +0100

    fix mask marking and component highlight (#477)

--- package/PartSeg/common_gui/napari_image_view.py
+++ package/PartSeg/common_gui/napari_image_view.py
@@ -344,7 +344,7 @@ class ImageView(QWidget):
     def mask_color(self) -> ColorInfo:
         """Get mask marking color"""
         color = Color(np.divide(self.settings.get_from_profile("mask_presentation_color", [255, 255, 255]), 255))
-        return {0: "black", 1: color.rgba}
+        return {0: (0, 0, 0, 0), 1: color.rgba}
 
     def get_image(self, image: Optional[Image]) -> Image:
         if image is not None:
@@ -775,7 +775,9 @@ class ImageView(QWidget):
         component_mark = image_info.roi_info.roi[tuple(slices)] == num
         if self.viewer.dims.ndisplay == 3:
             component_mark = binary_dilation(component_mark)
-        translate = image_info.roi.translate + (bound_info.lower - 1) * image_info.roi.scale
+        shift_base = bound_info.lower - 1
+        shift_base[0] += 1  # remove shift on time axis
+        translate = image_info.roi.translate + shift_base * image_info.roi.scale
         translate[image_info.image.stack_pos] = 0
         if image_info.highlight is None:
             image_info.highlight = self.viewer.add_labels(
