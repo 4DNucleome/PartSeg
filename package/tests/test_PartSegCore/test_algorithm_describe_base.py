f9934c33d35fe1f556bdd1a62e1ec3ae94e00a06
Grzegorz Bokota <bokota+github@gmail.com>
2022-02-01 12:09:10 +0100

    finish test for base types

--- package/tests/test_PartSegCore/test_algorithm_describe_base.py
+++ package/tests/test_PartSegCore/test_algorithm_describe_base.py
@@ -11,6 +11,7 @@ from PartSegCore.algorithm_describe_base import (
     _GetDescriptionClass,
     base_model_to_algorithm_property,
 )
+from PartSegCore.channel_class import Channel
 from PartSegCore.class_register import class_to_str
 
 
@@ -70,7 +71,7 @@ def test_algorithm_selection():
     assert TestSelection["test1"] is Class1
 
 
-def test_base_model_to_algorithm_property():
+def test_base_model_to_algorithm_property_base():
     class SampleEnum(Enum):
         a = 1
         b = 2
@@ -79,12 +80,13 @@ def test_base_model_to_algorithm_property():
         field1: int = Field(0, le=100, ge=0, title="Field 1")
         field2: SampleEnum = SampleEnum.a
         field3: float = Field(0, le=55, ge=-7, title="Field 3")
+        channel: Channel = Field(0, title="Channel")
 
     s = Sample(field1=1, field3=1.5)
     assert s.field3 == 1.5
 
     converted = base_model_to_algorithm_property(Sample)
-    assert len(converted) == 3
+    assert len(converted) == 4
     assert converted[0].name == "field1"
     assert converted[0].user_name == "Field 1"
     assert issubclass(converted[0].value_type, int)
@@ -96,3 +98,7 @@ def test_base_model_to_algorithm_property():
     assert converted[2].user_name == "Field 3"
     assert issubclass(converted[2].value_type, float)
     assert converted[2].range == (-7, 55)
+
+    assert converted[3].value_type is Channel
+    assert converted[3].name == "channel"
+    assert converted[3].user_name == "Channel"
