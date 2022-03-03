8036fa861fbfb8ef19d97def5625f13530763dda
Grzegorz Bokota <bokota+github@gmail.com>
2022-03-03 23:53:37 +0100

    fix aborting

--- test_napari.sh
+++ test_napari.sh
@@ -2,9 +2,27 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-set -euo pipefail
-
 cd $SCRIPT_DIR
+
+
+
+abort()
+{
+    echo >&2 '
+***************
+*** ABORTED ***
+***************
+'
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+trap 'abort' 0
+
+set -e
+
 python -m pytest package/tests --no-cov
 python -m pytest package/tests --no-cov
 python -m pytest package/tests --no-cov
+
+trap : 0
