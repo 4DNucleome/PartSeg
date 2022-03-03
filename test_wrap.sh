c2eaa249587a79a93f5d8b711243d671d5d72aff
Grzegorz Bokota <bokota+github@gmail.com>
2022-03-03 20:01:58 +0100

    move to wrapper script

---
+++ test_wrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd napari
+pwd
+git status
+git tag
+git bisect start
+git bisect bad
+git bisect good v0.4.14
+git bisect
+git bisect run bash ../test_napari.sh
+git status
