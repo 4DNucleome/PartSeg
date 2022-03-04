86710602065e82800e838bfac2e52dc67cb18b6d
Grzegorz Bokota <bokota+github@gmail.com>
2022-03-04 23:59:13 +0100

    limit search range

--- test_wrap.sh
+++ test_wrap.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd napari
 git bisect start
-git bisect bad
+git bisect bad v0.4.15rc0
 git bisect good v0.4.14
 git bisect
 git bisect run bash ../test_napari.sh
