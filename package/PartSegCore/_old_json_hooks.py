cbeaf5ac2cdad6773e769f4a349aab4448862d98
Grzegorz Bokota <bokota+github@gmail.com>
2022-03-28 21:46:20 +0200

    do not allow exception to break application on startup

--- package/PartSegCore/_old_json_hooks.py
+++ package/PartSegCore/_old_json_hooks.py
@@ -1,10 +1,8 @@
-import importlib
-
 import numpy as np
 from napari.utils import Colormap
 
 from PartSegCore.algorithm_describe_base import ROIExtractionProfile
-from PartSegCore.class_generator import SerializeClassEncoder, serialize_hook
+from PartSegCore.class_generator import SerializeClassEncoder
 from PartSegCore.image_operations import RadiusType
 
 
@@ -65,30 +63,36 @@ def part_hook(dkt):
     from PartSegCore.analysis.calculation_plan import CalculationPlan, CalculationTree
     from PartSegCore.analysis.measurement_calculation import MeasurementProfile
 
-    if "__StatisticProfile__" in dkt:
-        del dkt["__StatisticProfile__"]
-        res = MeasurementProfile(**dkt)
-        return res
-    if "__MeasurementProfile__" in dkt:
-        del dkt["__MeasurementProfile__"]
-        res = MeasurementProfile(**dkt)
-        return res
-    if "__CalculationPlan__" in dkt:
-        del dkt["__CalculationPlan__"]
-        return CalculationPlan(**dkt)
-    if "__CalculationTree__" in dkt:
-        return CalculationTree(operation=dkt["operation"], children=dkt["children"])
-    if (
-        "__subtype__" in dkt
-        and "statistic_profile" in dkt
-        and dkt["__subtype__"]
-        in (
-            "PartSegCore.analysis.calculation_plan.MeasurementCalculate",
-            "PartSeg.utils.analysis.calculation_plan.StatisticCalculate",
-        )
-    ):
-        dkt["measurement_profile"] = dkt["statistic_profile"]
-        del dkt["statistic_profile"]
+    dkt2 = dkt.copy()
+    try:
+        if "__StatisticProfile__" in dkt:
+            del dkt["__StatisticProfile__"]
+            res = MeasurementProfile(**dkt)
+            return res
+        if "__MeasurementProfile__" in dkt:
+            del dkt["__MeasurementProfile__"]
+            res = MeasurementProfile(**dkt)
+            return res
+        if "__CalculationPlan__" in dkt:
+            del dkt["__CalculationPlan__"]
+            return CalculationPlan(**dkt)
+        if "__CalculationTree__" in dkt:
+            return CalculationTree(operation=dkt["operation"], children=dkt["children"])
+        if (
+            "__subtype__" in dkt
+            and "statistic_profile" in dkt
+            and dkt["__subtype__"]
+            in (
+                "PartSegCore.analysis.calculation_plan.MeasurementCalculate",
+                "PartSeg.utils.analysis.calculation_plan.StatisticCalculate",
+            )
+        ):
+            dkt["measurement_profile"] = dkt["statistic_profile"]
+            del dkt["statistic_profile"]
+    except Exception as e:  # pylint: disable=W0703
+        dkt = dkt2
+        dkt["__error__"] = str(e)
+
     return profile_hook(dkt)
 
 
@@ -101,56 +105,51 @@ def profile_hook(dkt):
     ...     data = json.load(fp, object_hook=profile_hook)
 
     """
-    if "__class__" in dkt:
-        module_name, class_name = dkt["__class__"].rsplit(".", maxsplit=1)
-        # the migration code should be called here
-        try:
-            del dkt["__class__"]
-            module = importlib.import_module(module_name)
-            return getattr(module, class_name)(**dkt)
-        except Exception as e:  # skipcq: PTC-W0703`  # pylint: disable=W0703    # pragma: no cover
-            dkt["__class__"] = f"{module_name}.{class_name}"
-            dkt["__error__"] = e
-    if "__ProfileDict__" in dkt:
-        from PartSegCore.utils import ProfileDict
+    dkt2 = dkt.copy()
+    try:
+        if "__ProfileDict__" in dkt:
+            from PartSegCore.utils import ProfileDict
 
-        del dkt["__ProfileDict__"]
-        res = ProfileDict(**dkt)
-        return res
-    if "__RadiusType__" in dkt:
-        return RadiusType(dkt["value"])
-    if "__SegmentationProperty__" in dkt:
-        del dkt["__SegmentationProperty__"]
-        res = ROIExtractionProfile(**dkt)
-        return res
-    if "__SegmentationProfile__" in dkt:
-        del dkt["__SegmentationProfile__"]
-        res = ROIExtractionProfile(**dkt)
-        return res
-    if (
-        "__Serializable__" in dkt and dkt["__subtype__"] == "HistoryElement" and "algorithm_name" in dkt
-    ):  # pragma: no cover
-        # old code fix
-        name = dkt["algorithm_name"]
-        par = dkt["algorithm_values"]
-        del dkt["algorithm_name"]
-        del dkt["algorithm_values"]
-        dkt["segmentation_parameters"] = {"algorithm_name": name, "values": par}
-    if "__Serializable__" in dkt and dkt["__subtype__"] == "PartSegCore.color_image.base_colors.ColorMap":
-        positions, colors = list(zip(*dkt["colormap"]))
-        return Colormap(colors, controls=positions)
-    if "__Serializable__" in dkt and dkt["__subtype__"] == "PartSegCore.color_image.base_colors.ColorPosition":
-        return (dkt["color_position"], dkt["color"])
-    if "__Serializable__" in dkt and dkt["__subtype__"] == "PartSegCore.color_image.base_colors.Color":
-        return (dkt["red"] / 255, dkt["green"] / 255, dkt["blue"] / 255)
-    if "__Colormap__" in dkt:
-        del dkt["__Colormap__"]
-        if dkt["controls"][0] != 0:
-            dkt["controls"].insert(0, 0)
-            dkt["colors"].insert(0, dkt["colors"][0])
-        if dkt["controls"][-1] != 1:
-            dkt["controls"].append(1)
-            dkt["colors"].append(dkt["colors"][-1])
-        return Colormap(**dkt)
+            del dkt["__ProfileDict__"]
+            res = ProfileDict(**dkt)
+            return res
+        if "__RadiusType__" in dkt:
+            return RadiusType(dkt["value"])
+        if "__SegmentationProperty__" in dkt:
+            del dkt["__SegmentationProperty__"]
+            res = ROIExtractionProfile(**dkt)
+            return res
+        if "__SegmentationProfile__" in dkt:
+            del dkt["__SegmentationProfile__"]
+            res = ROIExtractionProfile(**dkt)
+            return res
+        if (
+            "__Serializable__" in dkt and dkt["__subtype__"] == "HistoryElement" and "algorithm_name" in dkt
+        ):  # pragma: no cover
+            # old code fix
+            name = dkt["algorithm_name"]
+            par = dkt["algorithm_values"]
+            del dkt["algorithm_name"]
+            del dkt["algorithm_values"]
+            dkt["segmentation_parameters"] = {"algorithm_name": name, "values": par}
+        if "__Serializable__" in dkt and dkt["__subtype__"] == "PartSegCore.color_image.base_colors.ColorMap":
+            positions, colors = list(zip(*dkt["colormap"]))
+            return Colormap(colors, controls=positions)
+        if "__Serializable__" in dkt and dkt["__subtype__"] == "PartSegCore.color_image.base_colors.ColorPosition":
+            return (dkt["color_position"], dkt["color"])
+        if "__Serializable__" in dkt and dkt["__subtype__"] == "PartSegCore.color_image.base_colors.Color":
+            return (dkt["red"] / 255, dkt["green"] / 255, dkt["blue"] / 255)
+        if "__Colormap__" in dkt:
+            del dkt["__Colormap__"]
+            if dkt["controls"][0] != 0:
+                dkt["controls"].insert(0, 0)
+                dkt["colors"].insert(0, dkt["colors"][0])
+            if dkt["controls"][-1] != 1:
+                dkt["controls"].append(1)
+                dkt["colors"].append(dkt["colors"][-1])
+            return Colormap(**dkt)
+    except Exception as e:  # pylint: disable=W0703
+        dkt = dkt2
+        dkt["__error__"] = str(e)
 
-    return serialize_hook(dkt)
+    return dkt
