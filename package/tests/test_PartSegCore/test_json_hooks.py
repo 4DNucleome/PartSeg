0076f312ba6da138c95463c75f25092eff06286f
Grzegorz Bokota <bokota+github@gmail.com>
2022-04-03 19:09:02 +0200

    more clean

--- package/tests/test_PartSegCore/test_json_hooks.py
+++ package/tests/test_PartSegCore/test_json_hooks.py
@@ -1,32 +1,8 @@
-# pylint: disable=R0201
-
 import json
-from dataclasses import dataclass
 
 from PartSegCore.image_operations import RadiusType
 from PartSegCore.json_hooks import PartSegEncoder, partseg_object_hook
-from PartSegCore.utils import BaseModel, ProfileDict
-
-
-@dataclass
-class SampleDataclass:
-    filed1: int
-    field2: str
-
-
-class SamplePydantic(BaseModel):
-    sample_int: int
-    sample_str: str
-    sample_dataclass: SampleDataclass
-
-
-class SampleAsDict:
-    def __init__(self, value1, value2):
-        self.value1 = value1
-        self.value2 = value2
-
-    def as_dict(self):
-        return {"value1": self.value1, "value2": self.value2}
+from PartSegCore.utils import ProfileDict
 
 
 def test_profile_hook_colormap_load(bundle_test_dir):
