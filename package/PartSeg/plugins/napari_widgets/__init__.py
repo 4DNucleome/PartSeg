03d9c1f6b2f5efc730044ce6d32474aab93ef589
Grzegorz Bokota <bokota+github@gmail.com>
2022-05-04 22:06:24 +0200

    fix: Register PartSeg plugins before start napari widgets. (#611)
    * fix itk mask save code
    
    * fix unperdicted behaviour when import old colormap
    
    * register partseg plugin in napari widgets

--- package/PartSeg/plugins/napari_widgets/__init__.py
+++ package/PartSeg/plugins/napari_widgets/__init__.py
@@ -1,6 +1,7 @@
 from napari_plugin_engine import napari_hook_implementation
 
 from PartSeg._launcher.main_window import PartSegGUILauncher
+from PartSeg.plugins import register as register_plugin
 from PartSeg.plugins.napari_widgets.mask_create_widget import MaskCreate
 from PartSeg.plugins.napari_widgets.roi_extraction_algorithms import ROIAnalysisExtraction, ROIMaskExtraction
 from PartSeg.plugins.napari_widgets.search_label_widget import SearchLabel
@@ -10,16 +11,19 @@ from PartSeg.plugins.napari_widgets.search_label_widget import SearchLabel
 def napari_experimental_provide_dock_widget():
     from PartSeg.plugins.napari_widgets.simple_measurement_widget import SimpleMeasurement
 
+    register_plugin()
     return SimpleMeasurement
 
 
 @napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
 def napari_experimental_provide_dock_widget1():
+    register_plugin()
     return ROIAnalysisExtraction
 
 
 @napari_hook_implementation(specname="napari_experimental_provide_dock_widget")
 def napari_experimental_provide_dock_widget2():
+    register_plugin()
     return ROIMaskExtraction
 
 
@@ -32,6 +36,7 @@ def napari_experimental_provide_dock_widget3():
 def napari_experimental_provide_dock_widget4():
     from PartSeg.plugins.napari_widgets.measurement_widget import Measurement
 
+    register_plugin()
     return Measurement
 
 
