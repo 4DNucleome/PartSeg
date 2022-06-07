895243b385d0d92a422e839ac44619a62252cbc2
Grzegorz Bokota <bokota+github@gmail.com>
2022-06-08 01:24:59 +0200

    fix environment clean script

--- build_utils/setup_cfg_to_yaml.sh
+++ build_utils/setup_cfg_to_yaml.sh
@@ -13,7 +13,7 @@ setup_cfg_block_to_yaml_list () {
         grep -E "^ +\w"                       | # (2)
         grep -v ";"                           |
         sed -r "s/^ +/  - /g"                 |    # (3)
-        sed 's/napari/"napari=*=*pyside2"/g'
+        sed 's/napari.*/"napari=*=*pyside2"/g'
 }
 
 setup_cfg_block_to_yaml_list "$1"
