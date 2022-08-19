6e41af48f92b67b81bde68d6294c8b1eaa94c74e
Grzegorz Bokota <bokota+github@gmail.com>
2022-08-19 13:09:27 +0200

    fix qt console

--- build_utils/setup_cfg_to_yaml.sh
+++ build_utils/setup_cfg_to_yaml.sh
@@ -13,7 +13,8 @@ setup_cfg_block_to_yaml_list () {
         grep -E "^ +\w"                       | # (2)
         grep -v ";"                           |
         sed -r "s/^ +/  - /g"                 |    # (3)
-        sed 's/napari.*/"napari=*=*pyside2"/g'
+        sed 's/napari.*/"napari=*=*pyside2"/g'|
+        sed `s/qtconsole/qtconsole-base/g`
 }
 
 setup_cfg_block_to_yaml_list "$1"
