71a16db79d35e0faa22e3c5a9f03e8397b5b04c4
Grzegorz Bokota <bokota+github@gmail.com>
2022-10-26 00:18:56 +0200

    fix code for convert to mask projects

--- examples/convert_tiff_mask_to_projets.py
+++ examples/convert_tiff_mask_to_projets.py
@@ -1,4 +1,5 @@
 import dataclasses
+import os
 import sys
 from glob import glob
 from os import path
@@ -11,15 +12,21 @@ from PartSegCore.mask.io_functions import LoadROIFromTIFF, LoadStackImage, SaveR
 def main():
     file_list = glob("2022-10-22/*/*.obsep")
     for image_path in tqdm(file_list):
-        project_tuple = LoadStackImage.load([image_path])
         tiff_roi_path = path.join(path.dirname(image_path), "segmentation.tif")
+        roi_path = path.join(path.dirname(image_path), "segmentation.seg")
+        if os.path.exists(roi_path):
+            continue
         if not path.exists(tiff_roi_path):
             print(f"Mask {tiff_roi_path} not found", file=sys.stderr)
             continue
-        mask_tuple = LoadROIFromTIFF.load([tiff_roi_path])
-        roi_path = path.join(path.dirname(image_path), "segmentation.seg")
-        project_tuple = dataclasses.replace(project_tuple, roi_info=mask_tuple.roi_info)
-        SaveROI.save(roi_path, project_tuple, SaveROIOptions(relative_path=True, mask_data=True))
+        try:
+            project_tuple = LoadStackImage.load([image_path])
+            mask_tuple = LoadROIFromTIFF.load([tiff_roi_path])
+
+            project_tuple = dataclasses.replace(project_tuple, roi_info=mask_tuple.roi_info)
+            SaveROI.save(roi_path, project_tuple, SaveROIOptions(relative_path=True, mask_data=True))
+        except Exception as e:
+            print(f"Error in {image_path}: {e}", file=sys.stderr)
 
 
 if __name__ == "__main__":
