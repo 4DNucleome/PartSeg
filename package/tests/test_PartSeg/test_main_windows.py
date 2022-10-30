02f2c7c7447c24f04ebf78fa0bef062839065d67
Grzegorz Bokota <bokota+github@gmail.com>
2022-10-30 08:52:43 +0100

    next tests

--- package/tests/test_PartSeg/test_main_windows.py
+++ package/tests/test_PartSeg/test_main_windows.py
@@ -46,6 +46,14 @@ class TestAnalysisMainWindow:
         main_window.advanced_window.close()
         qtbot.wait(50)
 
+    @pyside_skip
+    def test_change_theme(self, qtbot, tmpdir):
+        main_window = analysis_main_window.MainWindow(tmpdir, initial_image=False)
+        qtbot.addWidget(main_window)
+        assert main_window.raw_image.viewer.theme == "light"
+        main_window.settings.theme_name = "dark"
+        assert main_window.raw_image.viewer.theme == "dark"
+
 
 @pytest.fixture
 def analysis_options(qtbot, part_settings):
@@ -154,6 +162,14 @@ class TestMaskMainWindow:
         main_window = mask_main_window.MainWindow(tmpdir, initial_image=False)
         qtbot.addWidget(main_window)
         qtbot.wait(50)
+
+    @pyside_skip
+    def test_change_theme(self, qtbot, tmpdir):
+        main_window = mask_main_window.MainWindow(tmpdir, initial_image=False)
+        qtbot.addWidget(main_window)
+        assert main_window.image_view.viewer.theme == "light"
+        main_window.settings.theme_name = "dark"
+        assert main_window.image_view.viewer.theme == "dark"
 
 
 class TestLauncherMainWindow:
