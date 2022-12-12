7c4081d93fa19c448f89959b07af46224f7e4784
Grzegorz Bokota <bokota+github@gmail.com>
2022-12-12 21:48:16 +0100

    Show which packages are updated by automatic vorkflow (#837)

---
+++ build_utils/check_updated_packages.py
@@ -0,0 +1,35 @@
+import argparse
+import re
+import subprocess  # nosec
+import sys
+from configparser import ConfigParser
+from pathlib import Path
+
+name_re = re.compile(r"\w+")
+changed_name_re = re.compile(r"\+(\w+)")
+
+
+src_dir = Path(__file__).parent.parent
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--main-packages", action="store_true")
+args = parser.parse_args()
+
+
+out = subprocess.run(  # nosec
+    ["git", "diff", str(src_dir / "requirements" / "requirements_pyinstaller.txt")], capture_output=True, check=True
+)
+
+changed_packages = [changed_name_re.match(x)[1] for x in out.stdout.decode().split("\n") if changed_name_re.match(x)]
+
+if not args.main_packages:
+    print("\n".join(f" * {x}" for x in sorted(changed_packages)))
+    sys.exit(0)
+
+
+config = ConfigParser()
+config.read(src_dir / "setup.cfg")
+packages = config["options"]["install_requires"].split("\n")
+packages = [name_re.match(package).group() for package in packages if name_re.match(package)]
+
+print(", ".join(f"`{x}`" for x in sorted(set(packages) & set(changed_packages))))
