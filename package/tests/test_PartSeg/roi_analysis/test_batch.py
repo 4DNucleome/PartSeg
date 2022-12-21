c69f3b900233c319398da70df194f9774b95abbc
Grzegorz Bokota <bokota+github@gmail.com>
2022-12-21 22:52:02 +0100

    simplify test

--- package/tests/test_PartSeg/roi_analysis/test_batch.py
+++ package/tests/test_PartSeg/roi_analysis/test_batch.py
@@ -1,41 +1,47 @@
+import pytest
+
 from PartSeg._roi_analysis.batch_window import CalculationPrepare
 from PartSegCore.analysis.batch_processing.batch_backend import CalculationManager
 from PartSegCore.analysis.calculation_plan import CalculationPlan, CalculationTree, MaskSuffix, RootType
 
 
-class TestCalculationPrepare:
-    def test_init(self, part_settings, qtbot, tmp_path):
-        files = [tmp_path / "test1.tif", tmp_path / "test2.tif"]
-        for file in files:
-            file.write_text("test")
+@pytest.fixture
+def calculation_prepare(tmp_path, part_settings, qtbot):
+    def _constructor(file_list, calculation_plan=None):
+        if calculation_plan is None:
+            calculation_plan = CalculationPlan()
         dial = CalculationPrepare(
-            file_list=files,
-            calculation_plan=CalculationPlan(),
+            file_list=file_list,
+            calculation_plan=calculation_plan,
             measurement_file_path=tmp_path / "test1.xlsx",
             settings=part_settings,
             batch_manager=CalculationManager(),
         )
         qtbot.addWidget(dial)
+        return dial
+
+    return _constructor
+
+
+class TestCalculationPrepare:
+    def test_init(self, calculation_prepare, tmp_path):
+        files = [tmp_path / "test1.tif", tmp_path / "test2.tif"]
+        for file in files:
+            file.write_text("test")
+        dial = calculation_prepare(file_list=files)
         assert dial.execute_btn.isEnabled()
 
-    def test_no_file(self, part_settings, qtbot, tmp_path):
+    def test_no_file(self, calculation_prepare, tmp_path):
         files = [tmp_path / "test1.tif", tmp_path / "test2.tif"]
         for file in files:
             file.write_text("test")
 
         files.append(tmp_path / "test3.tif")
 
-        dial = CalculationPrepare(
-            file_list=files,
-            calculation_plan=CalculationPlan(),
-            measurement_file_path=tmp_path / "test1.xlsx",
-            settings=part_settings,
-            batch_manager=CalculationManager(),
-        )
-        qtbot.addWidget(dial)
+        dial = calculation_prepare(file_list=files)
         assert not dial.execute_btn.isEnabled()
 
-    def test_no_mask(self, part_settings, qtbot, tmp_path):
+    def test_no_mask(self, calculation_prepare, tmp_path):
         files = [tmp_path / "test1.tif", tmp_path / "test2.tif"]
         for file in files:
             file.write_text("test")
@@ -44,20 +50,16 @@ class TestCalculationPrepare:
         files.append(tmp_path / "test3.tif")
         files[-1].write_text("test")
 
-        dial = CalculationPrepare(
+        dial = calculation_prepare(
             file_list=files,
             calculation_plan=CalculationPlan(
                 CalculationTree(RootType.Image, [CalculationTree(MaskSuffix(name="", suffix="_mask"), [])])
             ),
-            measurement_file_path=tmp_path / "test1.xlsx",
-            settings=part_settings,
-            batch_manager=CalculationManager(),
         )
-        qtbot.addWidget(dial)
         dial._check_start_conditions()
         assert not dial.execute_btn.isEnabled()
 
-    def test_choose_data_prefix(self, part_settings, qtbot, tmp_path, monkeypatch):
+    def test_choose_data_prefix(self, calculation_prepare, tmp_path, monkeypatch):
         files = [tmp_path / "test1.tif", tmp_path / "test2.tif"]
         for file in files:
             file.write_text("test")
@@ -65,19 +67,12 @@ class TestCalculationPrepare:
         monkeypatch.setattr(
             "PartSeg._roi_analysis.batch_window.QFileDialog.selectedFiles", lambda *_: [tmp_path / "test_dir"]
         )
-        dial = CalculationPrepare(
-            file_list=files,
-            calculation_plan=CalculationPlan(),
-            measurement_file_path=tmp_path / "test1.xlsx",
-            settings=part_settings,
-            batch_manager=CalculationManager(),
-        )
-        qtbot.addWidget(dial)
+        dial = calculation_prepare(file_list=files)
         dial.choose_data_prefix()
 
         assert dial.base_prefix.text() == str(tmp_path / "test_dir")
 
-    def test_choose_result_prefix(self, part_settings, qtbot, tmp_path, monkeypatch):
+    def test_choose_result_prefix(self, calculation_prepare, tmp_path, monkeypatch):
         files = [tmp_path / "test1.tif", tmp_path / "test2.tif"]
         for file in files:
             file.write_text("test")
@@ -85,14 +80,7 @@ class TestCalculationPrepare:
         monkeypatch.setattr(
             "PartSeg._roi_analysis.batch_window.QFileDialog.selectedFiles", lambda *_: [tmp_path / "test_dir"]
         )
-        dial = CalculationPrepare(
-            file_list=files,
-            calculation_plan=CalculationPlan(),
-            measurement_file_path=tmp_path / "test1.xlsx",
-            settings=part_settings,
-            batch_manager=CalculationManager(),
-        )
-        qtbot.addWidget(dial)
+        dial = calculation_prepare(file_list=files)
         dial.choose_result_prefix()
 
         assert dial.result_prefix.text() == str(tmp_path / "test_dir")
