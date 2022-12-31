58cf3f819f552a941e2916c31ec16fd7f633f11b
Sourcery AI <>
2022-12-31 20:21:03 +0000

    'Refactored by Sourcery'

--- package/PartSegCore/napari_plugins/load_masked_image.py
+++ package/PartSegCore/napari_plugins/load_masked_image.py
@@ -9,7 +9,12 @@ from PartSegCore.napari_plugins.loader import partseg_loader
 
 @napari_hook_implementation
 def napari_get_reader(path: str):
-    for extension in LoadStackImageWithMask.get_extensions():
-        if path.endswith(extension) and os.path.exists(LoadStackImageWithMask.get_next_file([path])):
-            return functools.partial(partseg_loader, LoadStackImageWithMask)
-    return None
+    return next(
+        (
+            functools.partial(partseg_loader, LoadStackImageWithMask)
+            for extension in LoadStackImageWithMask.get_extensions()
+            if path.endswith(extension)
+            and os.path.exists(LoadStackImageWithMask.get_next_file([path]))
+        ),
+        None,
+    )
--- package/PartSegCore/napari_plugins/load_roi_project.py
+++ package/PartSegCore/napari_plugins/load_roi_project.py
@@ -9,7 +9,12 @@ from PartSegCore.napari_plugins.loader import partseg_loader
 
 @napari_hook_implementation
 def napari_get_reader(path: str):
-    for extension in LoadProject.get_extensions():
-        if path.endswith(extension) and os.path.exists(LoadProject.get_next_file([path])):
-            return functools.partial(partseg_loader, LoadProject)
-    return None
+    return next(
+        (
+            functools.partial(partseg_loader, LoadProject)
+            for extension in LoadProject.get_extensions()
+            if path.endswith(extension)
+            and os.path.exists(LoadProject.get_next_file([path]))
+        ),
+        None,
+    )
