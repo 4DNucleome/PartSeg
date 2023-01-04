ef5badb5791d07f8152d60cb4d1889137302096b
Grzegorz Bokota <bokota+github@gmail.com>
2023-01-04 16:01:37 +0100

    fix minimal req pipeline

--- build_utils/create_minimal_req.py
+++ build_utils/create_minimal_req.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 from configparser import ConfigParser
 
 config = ConfigParser()
@@ -7,7 +8,19 @@ config.read(os.path.join(os.path.dirname(__file__), "..", "setup.cfg"))
 minimum_requirements = config["options"]["install_requires"].replace(">=", "==")
 minimum_qt = config["options.extras_require"]["pyqt"].replace(">=", "==")
 
-with open(os.path.join(os.path.dirname(__file__), "minimal-req.txt"), "w") as f_p:
+with open(os.path.join(os.path.dirname(__file__), "minimal-req.in"), "w") as f_p:
     f_p.write(minimum_requirements)
     f_p.write(minimum_qt)
+    f_p.write("\nnpe2==0.1.1")
     f_p.write("\n")
+
+subprocess.run(
+    [
+        "pip-compile",
+        "--resolver=backtracking",
+        os.path.join(os.path.dirname(__file__), "minimal-req.in"),
+        "-o",
+        os.path.join(os.path.dirname(__file__), "minimal-req.txt"),
+        "-q",
+    ]
+)
