449328f6b82b9c501e4d934390736e7773477390
Grzegorz Bokota <bokota+github@gmail.com>
2023-03-14 21:46:11 +0100

    fix custom_name_generate

--- package/PartSegCore/custom_name_generate.py
+++ package/PartSegCore/custom_name_generate.py
@@ -8,5 +8,4 @@ def custom_name_generate(prohibited_names: Set[str], dict_names: Dict[str, Any])
         rand_name = "custom_" + "".join(random.choice(string.ascii_letters + string.digits) for _ in range(10))  # nosec
         if rand_name not in prohibited_names and rand_name not in dict_names:
             return rand_name
-        raise RuntimeError("Cannot generate proper names")  # pragma: no cover
-    return None
+    raise RuntimeError("Cannot generate proper names")  # pragma: no cover
