bd8db2c4933025ebef27903c253ab1942167923a
Grzegorz Bokota <bokota+github@gmail.com>
2023-04-04 14:50:24 +0200

    fix: Update pyinstaller configuration (#926)

--- package/PartSeg/launcher_main.py
+++ package/PartSeg/launcher_main.py
@@ -11,7 +11,7 @@ multiprocessing.freeze_support()
 
 
 # noinspection PyUnresolvedReferences,PyUnusedLocal
-def _test_imports():
+def _test_imports():  # pragma: no cover
     print("start_test_import")
     from qtpy.QtWidgets import QApplication
 
@@ -25,6 +25,11 @@ def _test_imports():
     from PartSeg._roi_analysis.main_window import MainWindow as AnalysisMain
     from PartSeg._roi_mask.main_window import MainWindow as MaskMain
     from PartSeg.common_backend.base_argparser import _setup_sentry
+
+    with suppress(ImportError):
+        from napari.qt import get_app
+
+        get_app()
 
     _setup_sentry()
     freetype.get_handle()
