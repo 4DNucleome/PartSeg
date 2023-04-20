8c55cdddf2356bbc896059915a024678f85d79d4
Grzegorz Bokota <bokota+github@gmail.com>
2023-04-20 23:45:29 +0200

    fix deepsource warnings

--- examples/dev/update_frame_thick.py
+++ examples/dev/update_frame_thick.py
@@ -40,8 +40,9 @@ def process_files(args):
     from PartSegCore.json_hooks import PartSegEncoder
 
     files = list(glob(f"{args.input}/*.seg"))
-    if not len(files):
-        raise print(f"No files in {args.input}/*.seg", file=sys.stderr)
+    if not files:
+        print(f"No files in {args.input}/*.seg", file=sys.stderr)
+        return
 
     for file_path in tqdm(files):
         path = Path(file_path)
@@ -58,7 +59,7 @@ def process_files(args):
                 meta_buff = io.BytesIO(json.dumps(metadata, cls=PartSegEncoder).encode("utf8"))
                 f.addfile(get_tarinfo("metadata.json", meta_buff), meta_buff)
                 f.addfile(get_tarinfo("segmentation.tif", segmentation_buff), segmentation_buff)
-        except:
+        except:  # noqa: E722 RUF100
             shutil.move(new_path, file_path)
             raise
         shutil.move(new_path, new_path.with_suffix(".seg_old"))
