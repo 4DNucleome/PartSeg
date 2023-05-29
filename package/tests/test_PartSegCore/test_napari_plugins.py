5a2957fc131df5942c490e51fbad64b0e11a3715
Grzegorz Bokota <bokota+github@gmail.com>
2023-05-29 15:08:27 +0200

    fix: Fix bug in the wrong order of axis saved in napari contribution (#972)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- package/tests/test_PartSegCore/test_napari_plugins.py
+++ package/tests/test_PartSegCore/test_napari_plugins.py
@@ -6,12 +6,14 @@ import numpy as np
 import pytest
 from napari.layers import Image, Labels, Layer
 
+from PartSegCore.mask.io_functions import LoadROIFromTIFF
 from PartSegCore.napari_plugins.load_image import napari_get_reader as napari_get_reader_image
 from PartSegCore.napari_plugins.load_mask_project import napari_get_reader as napari_get_reader_mask
 from PartSegCore.napari_plugins.load_masked_image import napari_get_reader as napari_get_reader_mask_image
 from PartSegCore.napari_plugins.load_roi_project import napari_get_reader as napari_get_reader_roi
 from PartSegCore.napari_plugins.loader import project_to_layers
 from PartSegCore.napari_plugins.save_mask_roi import napari_write_labels
+from PartSegCore.napari_plugins.save_tiff_layer import napari_write_labels as napari_write_labels_tiff
 
 
 def test_project_to_layers_analysis(analysis_segmentation):
@@ -91,3 +93,12 @@ def test_write_labels(tmp_path):
 
     assert napari_write_labels(str(tmp_path / "test.txt"), data, {"scale": [10, 10]}) is None
     assert not (tmp_path / "test.txt").exists()
+
+
+def test_save_load_axis_order(tmp_path):
+    data = np.zeros((1, 10, 20, 30), dtype=np.uint8)
+    layer = Labels(data)
+    data_path = str(tmp_path / "test.tif")
+    assert napari_write_labels_tiff(data_path, *layer.as_layer_data_tuple()[:2])
+    proj = LoadROIFromTIFF.load([data_path])
+    assert proj.roi_info.roi.shape == data.shape
