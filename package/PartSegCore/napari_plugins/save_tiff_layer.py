b38933b84ea47fe2b2235087a72081833ebf930d
Grzegorz Bokota <bokota+github@gmail.com>
2023-07-31 09:30:15 +0200

    feat: allow to save multiple napari image layers to single tiff file (#1000)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- package/PartSegCore/napari_plugins/save_tiff_layer.py
+++ package/PartSegCore/napari_plugins/save_tiff_layer.py
@@ -1,7 +1,8 @@
 import os
-from typing import Any, Optional
+from typing import Any, List, Optional
 
 import numpy as np
+from napari.types import FullLayerData
 
 from PartSegImage import Image, ImageWriter
 from PartSegImage.image import DEFAULT_SCALE_FACTOR
@@ -24,24 +25,36 @@ def napari_write_labels(path: str, data: Any, meta: dict) -> Optional[str]:
     return path
 
 
-def napari_write_image(path: str, data: Any, meta: dict) -> Optional[str]:
+def napari_write_images(path: str, layer_data: List[FullLayerData]) -> List[str]:
     ext = os.path.splitext(path)[1]
-    if not isinstance(data, np.ndarray) or ext not in {".tiff", ".tif", ".TIFF", ".TIF"}:
-        return None
-    scale_shift = min(data.ndim, 3)
+    base_shape = layer_data[0][0].shape
+    if not all(isinstance(x[0], np.ndarray) and x[0].shape == base_shape for x in layer_data) or ext.lower() not in {
+        ".tiff",
+        ".tif",
+    }:
+        return []
+    scale_shift = min(len(base_shape), 3)
     axes = "TZXY"
-    channel_names = [meta["name"]]
-    if data.shape[-1] < 6:
-        axes += "C"
+    channel_names = [meta["name"] for _, meta, _ in layer_data]
+    meta = layer_data[0][1]
+    if len(layer_data) == 1:
+        data = layer_data[0][0]
+        if data.shape[-1] < 6:
+            axes += "C"
+            scale_shift -= 1
+            channel_names = [f'{meta["name"]} {i}' for i in range(1, data.shape[-1] + 1)]
+        axes = axes[-data.ndim :]
+    else:
+        data = [x[0] for x in layer_data]
+        axes = f"C{axes[-len(data[0].shape):]}"
         scale_shift -= 1
-        channel_names = [f'{meta["name"]} {i}' for i in range(1, data.shape[-1] + 1)]
     image = Image(
         data,
         np.divide(meta["scale"], DEFAULT_SCALE_FACTOR)[-scale_shift:],
-        axes_order=axes[-data.ndim :],
+        axes_order=axes,
         channel_names=channel_names,
         shift=np.divide(meta["translate"], DEFAULT_SCALE_FACTOR)[-scale_shift:],
         name="Image",
     )
     ImageWriter.save(image, path)
-    return path
+    return [path]
