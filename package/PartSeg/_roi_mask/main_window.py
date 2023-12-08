79b1564a5957540cf3f6800ee1e28d0945253100
Grzegorz Bokota <bokota+github@gmail.com>
2023-12-08 21:37:36 +0100

    refactor: Fix qt flags in roi mask code (#1042)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Refactor**
    - Updated event and modifier constants to align with the latest
    standards in the user interface library.
    
    - **Bug Fixes**
    - Ensured consistent behavior of keyboard and window event handling
    across the application.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- package/PartSeg/_roi_mask/main_window.py
+++ package/PartSeg/_roi_mask/main_window.py
@@ -211,11 +211,11 @@ class MainMenu(BaseMainMenu):
                 self,
                 "Not supported",
                 "Time data are currently not supported. Maybe You would like to treat time as z-stack",
-                QMessageBox.Yes | QMessageBox.No,
-                QMessageBox.No,
+                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+                QMessageBox.StandardButton.No,
             )
 
-            if res == QMessageBox.Yes:
+            if res == QMessageBox.StandardButton.Yes:
                 image = image.swap_time_and_stack()
             else:
                 return False
@@ -314,7 +314,9 @@ class MainMenu(BaseMainMenu):
         save_location, _selected_filter, save_class, values = dial.get_result()
 
         def exception_hook(exception):
-            QMessageBox.critical(self, "Save error", f"Error on disc operation. Text: {exception}", QMessageBox.Ok)
+            QMessageBox.critical(
+                self, "Save error", f"Error on disc operation. Text: {exception}", QMessageBox.StandardButton.Ok
+            )
             raise exception
 
         dial = ExecuteFunctionDialog(
@@ -326,8 +328,12 @@ class MainMenu(BaseMainMenu):
         dial.exec_()
 
     def save_result(self):
-        if self.settings.image_path is not None and QMessageBox.Yes == QMessageBox.question(
-            self, "Copy", "Copy name to clipboard?", QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes
+        if self.settings.image_path is not None and QMessageBox.StandardButton.Yes == QMessageBox.question(
+            self,
+            "Copy",
+            "Copy name to clipboard?",
+            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+            QMessageBox.StandardButton.Yes,
         ):
             clipboard = QGuiApplication.clipboard()
             clipboard.setText(os.path.splitext(os.path.basename(self.settings.image_path))[0])
@@ -354,17 +360,19 @@ class MainMenu(BaseMainMenu):
         if conflict:
             # TODO modify because of long lists
             conflict_str = "\n".join(conflict)
-            if QMessageBox.No == QMessageBox.warning(
+            if QMessageBox.StandardButton.No == QMessageBox.warning(
                 self,
                 "Overwrite",
                 f"Overwrite files:\n {conflict_str}",
-                QMessageBox.Yes | QMessageBox.No,
-                QMessageBox.No,
+                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
+                QMessageBox.StandardButton.No,
             ):
                 self.save_result()
 
         def exception_hook(exception):
-            QMessageBox.critical(self, "Save error", f"Error on disc operation. Text: {exception}", QMessageBox.Ok)
+            QMessageBox.critical(
+                self, "Save error", f"Error on disc operation. Text: {exception}", QMessageBox.StandardButton.Ok
+            )
 
         dial = ExecuteFunctionDialog(
             res.save_class.save,
@@ -392,7 +400,7 @@ class ComponentCheckBox(QCheckBox):
 
 class ChosenComponents(QWidget):
     """
-    :type check_box: dict[int, QCheckBox]
+    :type check_box: dict[int, ComponentCheckBox]
     """
 
     check_change_signal = Signal()
@@ -769,7 +777,7 @@ class ImageInformation(QWidget):
         super().__init__(parent)
         self._settings = settings
         self.path = QTextEdit("<b>Path:</b> example image")
-        self.path.setWordWrapMode(QTextOption.WrapAnywhere)
+        self.path.setWordWrapMode(QTextOption.WrapMode.WrapAnywhere)
         self.path.setReadOnly(True)
         self.setMinimumHeight(20)
         self.spacing = [QDoubleSpinBox() for _ in range(3)]
@@ -785,8 +793,8 @@ class ImageInformation(QWidget):
         self.sync_dirs.stateChanged.connect(self.set_sync_dirs)
         units_value = self._settings.get("units_value", Units.nm)
         for el in self.spacing:
-            el.setAlignment(Qt.AlignRight)
-            el.setButtonSymbols(QAbstractSpinBox.NoButtons)
+            el.setAlignment(Qt.AlignmentFlag.AlignRight)
+            el.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
             el.setRange(0, 100000)
             # noinspection PyUnresolvedReferences
             el.valueChanged.connect(self.image_spacing_change)
@@ -898,7 +906,7 @@ class MainWindow(BaseMainWindow):
         self.image_view = StackImageView(self.settings, self.channel_control, name="channelcontrol")
         self.image_view.setMinimumWidth(450)
         self.info_text = QLabel()
-        self.info_text.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Preferred)
+        self.info_text.setSizePolicy(QSizePolicy.Policy.Ignored, QSizePolicy.Policy.Preferred)
         self.image_view.text_info_change.connect(self.info_text.setText)
         self.options_panel = Options(self.settings, self.image_view)
         self.main_menu = MainMenu(self.settings, self)
@@ -930,7 +938,7 @@ class MainWindow(BaseMainWindow):
         view_menu.addAction("Toggle scale bar").triggered.connect(self._toggle_scale_bar)
         action = view_menu.addAction("Screenshot")
         action.triggered.connect(self.screenshot(self.image_view))
-        action.setShortcut(QKeySequence.Print)
+        action.setShortcut(QKeySequence.StandardKey.Print)
         image_menu = menu_bar.addMenu("Image operations")
         image_menu.addAction("Image adjustment").triggered.connect(self.image_adjust_exec)
         help_menu = menu_bar.addMenu("Help")
