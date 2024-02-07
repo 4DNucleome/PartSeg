fca53dc01b6ab24caf2b33a37d8b917f64b5fae5
Grzegorz Bokota <bokota+github@gmail.com>
2024-02-07 15:40:28 +0100

    fix indent

--- build_utils/pyproject_toml_to_yaml.py
+++ build_utils/pyproject_toml_to_yaml.py
@@ -8,7 +8,7 @@ pyproject_toml = base_dir / "pyproject.toml"
 with pyproject_toml.open("rb") as f:
     data = tomllib.load(f)
 dependencies = data["project"]["dependencies"]
-dependencies_str = "\n".join([f" - {v}" for v in dependencies])
+dependencies_str = "\n".join([f"  - {v}" for v in dependencies])
 dependencies_str = dependencies_str.replace("napari", "napari=*=*pyside2")
 dependencies_str = dependencies_str.replace("qtconsole", "qtconsole-base")
 print(dependencies_str)
