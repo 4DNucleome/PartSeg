b5193ae57f54e3b74f5d88addf4ee6668a309c17
Grzegorz Bokota <bokota+github@gmail.com>
2024-02-07 23:07:16 +0100

    refactor: Migrate from setup.cfg to `pyproject.toml` (#1070)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    
    - **Chores**
    - Updated pre-commit hooks, removing `setup-cfg-fmt` and upgrading
    `ruff-pre-commit` to version `v0.2.0`.
    - Updated Python version in the workflow configuration from "3.9" to
    "3.11".
    - Replaced dependency extraction method with a Python script to convert
    `pyproject.toml` file to YAML format.
    - Dynamically generated dependencies using a Python script instead of a
    shell script, updating Python version to `3.11` in the generated
    `environment.yml` file.
    - Added `pyproject_toml_to_yaml.py` to convert `pyproject.toml`
    dependencies to YAML with modifications for specific dependencies.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ build_utils/pyproject_toml_to_yaml.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+import tomllib
+
+base_dir = Path(__file__).parent.parent
+pyproject_toml = base_dir / "pyproject.toml"
+
+with pyproject_toml.open("rb") as f:
+    data = tomllib.load(f)
+dependencies = data["project"]["dependencies"]
+dependencies_str = "\n".join([f"  - {v}" for v in dependencies])
+dependencies_str = dependencies_str.replace("qtconsole", "qtconsole-base")
+print(dependencies_str)
