3e25278dd673e52b45338b7bec93ecf32796dfdd
Grzegorz Bokota <bokota+github@gmail.com>
2024-03-04 16:00:32 +0100

    deduplicat type verificaton code

--- package/PartSegImage/channel_class.py
+++ package/PartSegImage/channel_class.py
@@ -12,27 +12,25 @@ if TYPE_CHECKING:
     from pydantic_core.core_schema import CoreSchema
 
 
+def check_type(value):  # type: ignore [misc]
+    if isinstance(value, Channel):
+        return value
+    if value.__class__.__module__.startswith("napari"):
+        value = value.name
+    if not isinstance(value, (str, int)):
+        raise TypeError(f"Channel need to be int or str, provided {type(value)}")
+    return Channel(value)
+
+
 if PYDANTIC_2:
 
-    def check_type(value, _validation_info=None, **_):
-        if isinstance(value, Channel):
-            return value
-        if value.__class__.__module__.startswith("napari"):
-            value = value.name
-        if not isinstance(value, (str, int)):
-            raise TypeError(f"Channel need to be int or str, provided {type(value)}")
-        return Channel(value)
+    def check_type_(value, _validation_info=None, **_):
+        return check_type(value)
 
 else:
 
-    def check_type(value):  # type: ignore [misc]
-        if isinstance(value, Channel):
-            return value
-        if value.__class__.__module__.startswith("napari"):
-            value = value.name
-        if not isinstance(value, (str, int)):
-            raise TypeError(f"Channel need to be int or str, provided {type(value)}")
-        return Channel(value)
+    def check_type_(value):  # type: ignore [misc]
+        return check_type(value)
 
 
 class Channel:
@@ -70,7 +68,7 @@ class Channel:
 
     @classmethod
     def __get_validators__(cls):
-        yield check_type
+        yield check_type_
 
     @classmethod
     def __modify_schema__(cls, field_schema):
