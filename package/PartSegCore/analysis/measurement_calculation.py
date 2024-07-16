fix: Fix preparing data for `mahotas.haralick` to avoid overflow problem (#1150)
Workaround for https://github.com/luispedro/mahotas/pull/150

<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

- **Bug Fixes**
- Improved image rescaling logic to enhance visual representation
accuracy.

- **Tests**
- Reordered test items for better organization and efficiency based on
file paths.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

package/PartSegCore/analysis/measurement_calculation.py
@@ -1533,9 +1533,11 @@ class HaralickEnum(Enum):
 def _rescale_image(data: np.ndarray):
     if data.dtype == np.uint8:
         return data
+    if np.issubdtype(data.dtype, np.integer) and data.min() >= 0 and data.max() < 255:
+        return data.astype(np.uint8)
     min_val = data.min()
     max_val = data.max()
-    return ((data - min_val) / ((max_val - min_val) / 255)).astype(np.uint8)
+    return ((data - min_val) / ((max_val - min_val) / 254)).astype(np.uint8)
 
 
 class HaralickParameters(BaseModel):