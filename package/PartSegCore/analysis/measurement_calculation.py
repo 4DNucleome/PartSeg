0f71824d5af77a6499441385ee4718599baf23d3
Grzegorz Bokota <bokota+github@gmail.com>
2024-07-16 13:00:28 +0200

    fix checking if integer

--- package/PartSegCore/analysis/measurement_calculation.py
+++ package/PartSegCore/analysis/measurement_calculation.py
@@ -1533,7 +1533,7 @@ class HaralickEnum(Enum):
 def _rescale_image(data: np.ndarray):
     if data.dtype == np.uint8:
         return data
-    if isinstance(data.dtype, np.integer) and data.min() >= 0 and data.max() < 255:
+    if np.issubdtype(data.dtype, np.integer) and data.min() >= 0 and data.max() < 255:
         return data.astype(np.uint8)
     min_val = data.min()
     max_val = data.max()
