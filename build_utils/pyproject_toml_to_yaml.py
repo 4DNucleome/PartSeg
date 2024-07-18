98af5f4072cb5c92faa9b0657060bd66c11fbc93
Grzegorz Bokota <bokota+github@gmail.com>
2024-07-18 21:31:50 +0200

    build: update install constraints on numpy and qt packages (#1157)
    Bump python version in some CI jobs 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
      - Updated support for Python 3.11 in Azure Pipelines.
      - Updated GitHub Actions workflow to support macOS 14 and Python 3.12.
    
    - **Dependencies**
    - Updated dependencies in `pyproject.toml` to conditionally include
    `numpy`, `napari`, and `PartSeg` based on Python versions.
    
    - **Tests**
    - Modified test fixture data type from `np.uint8` to `np.uint16` for
    enhanced precision.
    
    - **Refactor**
    - Added utility functions to streamline dependency specification
    handling in build utilities.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- build_utils/pyproject_toml_to_yaml.py
+++ build_utils/pyproject_toml_to_yaml.py
@@ -5,9 +5,27 @@ import tomllib
 base_dir = Path(__file__).parent.parent
 pyproject_toml = base_dir / "pyproject.toml"
 
+
+def drop_line(line):
+    if "python_version < '3.10'" in line:
+        return False
+
+    if "python_version < '3.11'" in line:  # noqa: SIM103
+        return False
+
+    return True
+
+
+def remove_specifier(line: str):
+    if ";" in line:
+        return line.split(";", maxsplit=1)[0]
+    return line
+
+
 with pyproject_toml.open("rb") as f:
     data = tomllib.load(f)
 dependencies = data["project"]["dependencies"]
+dependencies = map(remove_specifier, filter(drop_line, dependencies))
 dependencies_str = "\n".join([f"  - {v}" for v in dependencies])
 dependencies_str = dependencies_str.replace("qtconsole", "qtconsole-base")
 print(dependencies_str)
